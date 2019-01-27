d3e67f521612fafa7230cae3e63e0590d369d5bc
GalenWong <wonggalen1999@gmail.com>
2019-01-26 17:39:51 -0800

    First commit

---
+++ src/pages/playground.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class PlayGround extends React.Component {
+  render() {
+    return (
+      <h1>
+        This is a playground endpoint where you can show the newly built components 🔥
+      </h1>
+    );
+  }
+}
