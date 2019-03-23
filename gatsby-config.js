3e64bea08995ec1fe5ad4f9f0134e7b45f201ed8
GalenWong <wonggalen1999@gmail.com>
2019-03-23 00:41:19 -0700

    adding material-ui plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,7 @@ module.exports = {
       }
     },
     `gatsby-transformer-remark`,
+    `gatsby-plugin-material-ui`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
