f17046187bd924967865613b9a661cc2af32f993
Jody Lin <jody.m.lin@gmail.com>
2019-04-27 19:20:15 -0700

    Finish home banner

---
+++ src/components/homepage/wrenches.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import bigWrenches from './big_banner_wrenches.svg';
+import smallWrenches from './small_banner_wrenches.svg';
+
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+	bigWrench: {
+		maxWidth: '63%',
+		left: '5%',
+		[theme.breakpoints.down('xs')]: {
+			display: 'none'
+		}
+	},
+	smallWrench: {
+		width: '21vw',
+		marginLeft: '7vw',
+		display: 'none',
+		[theme.breakpoints.down('xs')]: {
+			display: 'block'
+		}
+	}
+});
+
+class Wrenches extends React.Component {
+	render() {
+		const { classes, className } = this.props;
+		return (
+			<div className={className}>
+				<img className={classes.bigWrench} src={bigWrenches}/>
+				<img className={classes.smallWrench} src={smallWrenches}/>
+			</div>
+		);
+	}
+}
+
+Wrenches.propTypes = {
+	classes: PropTypes.object.isRequired,
+	className: PropTypes.string
+};
+
+
+export default withStyles(styles)(Wrenches);
