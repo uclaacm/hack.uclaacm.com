8514d39347975559c61732724e3d536b3829c701
GalenWong <wonggalen1999@gmail.com>
2019-04-27 18:35:20 -0700

    markdown styling

--- src/components/post/postTemplate.js
+++ src/components/post/postTemplate.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
+import { withStyles } from '@material-ui/core/styles';
 
 import HeadFooter from '../headfooter/headfooter';
 import MDContainer from '../mdcontainer/mdcontainer';
+
+const styles = () => ({
+	container: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	post: {
+		maxWidth: '800px'
+	}
+});
 
 class PostTemplate extends React.Component {
 	render() {
 		// data.markdownRemark holds our post data
 		const { markdownRemark } = this.props.data;
 		const { frontmatter, html } = markdownRemark;
+		const { classes } = this.props;
 		return (
 			<HeadFooter>
-				<div className="blog-post-container">
-					<div className="blog-post">
+				<div className={classes.container}>
+					<div className={classes.post}>
 						<h1>{frontmatter.title}</h1>
 						<h2>{frontmatter.date}</h2>
 						<MDContainer html={html} />
@@ -25,10 +38,11 @@ class PostTemplate extends React.Component {
 }
 
 PostTemplate.propTypes = {
-	data: PropTypes.object.isRequired
+	data: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired
 };
 
-export default PostTemplate;
+export default withStyles(styles)(PostTemplate);
 
 export const pageQuery = graphql`
   query BlogPostByPath($path: String!) {
