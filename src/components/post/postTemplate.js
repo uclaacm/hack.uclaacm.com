aea541cab293340c4d79576f68b64b7b2c435399
GalenWong <wonggalen1999@gmail.com>
2019-05-08 20:21:07 -0700

    Added title and subtitle

--- src/components/post/postTemplate.js
+++ src/components/post/postTemplate.js
@@ -5,6 +5,7 @@ import { withStyles } from '@material-ui/core/styles';
 
 import HeadFooter from '../headfooter/headfooter';
 import MDContainer from '../mdcontainer/mdcontainer';
+import { Typography } from '@material-ui/core';
 
 const styles = theme => ({
 	container: {
@@ -15,6 +16,10 @@ const styles = theme => ({
 	post: {
 		maxWidth: '800px',
 		margin: theme.spacing.unit * 2
+	},
+	date: {
+		marginTop: theme.spacing.unit * 3,
+		marginBottom: theme.spacing.unit * 4
 	}
 });
 
@@ -24,12 +29,23 @@ class PostTemplate extends React.Component {
 		const { markdownRemark } = this.props.data;
 		const { frontmatter, html } = markdownRemark;
 		const { classes } = this.props;
+		const date = new Date(frontmatter.date).toLocaleDateString('en-US', {
+			year: 'numeric', month: 'long', day: 'numeric'
+		});
+
 		return (
 			<HeadFooter>
 				<div className={classes.container}>
 					<div className={classes.post}>
-						<h1>{frontmatter.title}</h1>
-						<h2>{frontmatter.date}</h2>
+						<Typography variant="h2">
+							{frontmatter.title}
+						</Typography>
+						<Typography variant="h4">
+							{frontmatter.subtitle}
+						</Typography>
+						<Typography variant="body1" className={classes.date}>
+							{date}
+						</Typography>
 						<MDContainer html={html} />
 					</div>
 				</div>
