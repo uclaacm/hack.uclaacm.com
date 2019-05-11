b6840778fe1c0aef3350c39a81d96b1ce5372ff4
Galen Wong <wonggalen1999@gmail.com>
2019-05-11 15:11:05 -0700

    Markdown Styling (#7)
    * Markdown image path resolution
    
    * markdown styling
    
    * Markdown container alignment
    
    * Update style
    
    * Added title and subtitle

--- src/components/post/postTemplate.js
+++ src/components/post/postTemplate.js
@@ -1,32 +1,65 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
+import { withStyles } from '@material-ui/core/styles';
+
+import HeadFooter from '../headfooter/headfooter';
+import MDContainer from '../mdcontainer/mdcontainer';
+import { Typography } from '@material-ui/core';
+
+const styles = theme => ({
+	container: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	post: {
+		maxWidth: '800px',
+		margin: theme.spacing.unit * 2
+	},
+	date: {
+		marginTop: theme.spacing.unit * 3,
+		marginBottom: theme.spacing.unit * 4
+	}
+});
 
 class PostTemplate extends React.Component {
 	render() {
 		// data.markdownRemark holds our post data
 		const { markdownRemark } = this.props.data;
 		const { frontmatter, html } = markdownRemark;
+		const { classes } = this.props;
+		const date = new Date(frontmatter.date).toLocaleDateString('en-US', {
+			year: 'numeric', month: 'long', day: 'numeric'
+		});
+
 		return (
-			<div className="blog-post-container">
-				<div className="blog-post">
-					<h1>{frontmatter.title}</h1>
-					<h2>{frontmatter.date}</h2>
-					<div
-						className="blog-post-content"
-						dangerouslySetInnerHTML={{ __html: html }}
-					/>
+			<HeadFooter>
+				<div className={classes.container}>
+					<div className={classes.post}>
+						<Typography variant="h2">
+							{frontmatter.title}
+						</Typography>
+						<Typography variant="h4">
+							{frontmatter.subtitle}
+						</Typography>
+						<Typography variant="body1" className={classes.date}>
+							{date}
+						</Typography>
+						<MDContainer html={html} />
+					</div>
 				</div>
-			</div>
+			</HeadFooter>
 		);
 	}
 }
 
 PostTemplate.propTypes = {
-	data: PropTypes.object.isRequired
+	data: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired
 };
 
-export default PostTemplate;
+export default withStyles(styles)(PostTemplate);
 
 export const pageQuery = graphql`
   query BlogPostByPath($path: String!) {
@@ -34,7 +67,8 @@ export const pageQuery = graphql`
       html
       frontmatter {
         date
-        title
+				title
+				subtitle
       }
     }
   }
