8dae2e8e42b560aff42a4eb64b29dcd26014320f
Jody Lin <jody.m.lin@gmail.com>
2019-06-21 20:53:18 -0700

    added homepagetemplate component

---
+++ src/components/homepage/homepagetemplate.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Banner from '../banner/banner';
+import HackDescription from '../hackdescription/hackdescription';
+
+import HeadFooter from '../headfooter/headfooter';
+export default class PlayGround extends React.Component {
+	render() {
+		return (
+			<HeadFooter>
+				<Banner />
+				<HackDescription />
+			</HeadFooter>
+		);
+	}
+}
