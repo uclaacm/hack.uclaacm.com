f920fb9991db0516e3e2f1fcb8f4d41625dbb86b
GalenWong <wonggalen1999@gmail.com>
2019-06-25 21:35:12 +0800

    Implement Tim's suggested changes
    - moved Cropper to a separate component
    - san-serif is actually sans-serif
    - using the `figure` html tag
    - remove some unnecessary CSS rules

--- src/components/photopage/profile.js
+++ src/components/photopage/profile.js
@@ -2,48 +2,34 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
-const cropperWidth = '200px';
+import Cropper from './cropper';
+
 const styles = theme => ({
 	container: {
 		display: 'flex',
 		flexDirection: 'column',
-		justifyContent: 'center',
 		alignItems: 'center',
 		padding: theme.spacing.unit * 2.75
 	},
-	// this cropper only crops landscape-oriented/square image
-	cropper: {
-		width: cropperWidth,
-		height: cropperWidth,
-		borderRadius: '50%',
-		overflow: 'hidden',
-		display: 'flex',
-		justifyContent: 'center'
-	},
-	photo: {
-		margin: '0 auto',
-		display: 'inline',
-		height: '100%',
-		width: 'auto'
+	caption: {
+		textAlign: 'center'
 	},
 	name: {
-		fontFamily: ['Chivo', 'san-serif'],
+		fontFamily: ['Chivo', 'sans-serif'],
 		fontSize: theme.typography.fontSize * 1.5,
-		fontWeight: 'normal',
 		margin: theme.spacing.unit,
 		// prevent long names from wrapping
 		whiteSpace: 'nowrap'
 	},
 	role: {
-		fontFamily: ['Chivo', 'san-serif'],
+		fontFamily: ['Chivo', 'sans-serif'],
 		fontSize: theme.typography.fontSize * 1.2,
-		fontWeight: 'normal',
 		margin: theme.spacing.unit,
 		// prevent long roles from wrapping
 		whiteSpace: 'nowrap'
 	},
 	description: {
-		fontFamily: ['Chivo', 'san-serif'],
+		fontFamily: ['Chivo', 'sans-serif'],
 		fontSize: theme.typography.fontSize,
 		fontWeight: 'lighter',
 		margin: theme.spacing.unit,
@@ -53,14 +39,14 @@ const styles = theme => ({
 
 function Profile({ classes, name, role, description, photoURL }) {
 	return (
-		<div className={classes.container}>
-			<div className={classes.cropper}>
-				<img src={photoURL} className={classes.photo}/>
-			</div>
-			<p className={classes.name}>{name}</p>
-			<p className={classes.role}>{role}</p>
-			<p className={classes.description}>{description}</p>
-		</div>
+		<figure className={classes.container}>
+			<Cropper photoURL={photoURL} />
+			<figcaption className={classes.caption}>
+				<p className={classes.name}>{name}</p>
+				<p className={classes.role}>{role}</p>
+				<p className={classes.description}>{description}</p>
+			</figcaption>
+		</figure>
 	);
 }
 
