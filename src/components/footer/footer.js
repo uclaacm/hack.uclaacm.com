c8dc1308252dc8616c1380d69fa8b25661d18def
Jody Lin <jody.m.lin@gmail.com>
2019-06-29 10:35:10 -0700

    fixed proptypes error

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -10,75 +10,75 @@ import medium from './medium.svg';
 import twitter from './twitter.svg';
 
 const styles = theme => ({
-    container: {
-        backgroundColor: '#352A3A'
-    },
-    link: {
-        textDecoration: 'none'
-    },
-    content: {
-        maxWidth: '1000px',
-        minHeight: '100px',
-        padding: '0 20px',
-        margin: 'auto',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        fontFamily: ['Poppins', 'sans-serif'],
-        fontSize: '16px',
-        color: '#FFFFFF',
-        textAlign: 'center',
-        [theme.breakpoints.down('xs')]: {
-            flexDirection: 'column'
-        }
-    },
-    icons: {
-        display: 'flex',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: '5px 0 0 0'
-    },
-    footerItem: {
-        width: 'max-content',
-        padding: '10px 0',
-        [theme.breakpoints.down('xs')]: {
-            width: '200px'
-        }
-    },
-    footerText: {
-        fontSize: '14px',
-    },
-    button: {
-        fontSize: '14px'
-    }
+	container: {
+		backgroundColor: '#352A3A'
+	},
+	buttonLink: {
+		textDecoration: 'none'
+	},
+	content: {
+		maxWidth: '1000px',
+		minHeight: '100px',
+		padding: '0 20px',
+		margin: 'auto',
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		fontFamily: ['Poppins', 'sans-serif'],
+		fontSize: '16px',
+		color: '#FFFFFF',
+		textAlign: 'center',
+		[theme.breakpoints.down('xs')]: {
+			flexDirection: 'column'
+		}
+	},
+	icons: {
+		display: 'flex',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		padding: '5px 0 0 0'
+	},
+	footerItem: {
+		width: 'max-content',
+		padding: '10px 0',
+		[theme.breakpoints.down('xs')]: {
+			width: '200px'
+		}
+	},
+	footerText: {
+		fontSize: '14px'
+	},
+	button: {
+		fontSize: '14px'
+	}
 });
 
 class Footer extends React.Component {
-    render () {
-        const { classes } = this.props;
-        return (
-            <div className={classes.container}> 
-                <div className={classes.content}>
-                    <div className={classes.footerItem}>
-                        <div className={classes.footerText}>FIND US ON SOCIAL MEDIA</div>
-                        <div className={classes.icons}>
-                            <Icon icon={facebook} link="https://www.facebook.com/groups/acmhack/"/>
-                            <Icon icon={github} link="https://github.com/uclaacm/"/>
-                            <Icon icon={medium} link="https://medium.com/techatucla"/>
-                            <Icon icon={twitter} link="https://twitter.com/uclaacm"/>
-                        </div>
-                    </div>
-                    <div className={classes.footerItem}>© ACM HACK 2019</div>
-                    <div className={classes.footerItem}>
-                        <a className={classes.link} href="http://eepurl.com/c5pE6P">
-                            <Button className={classes.button} variant="contained" >Join our Mailing List</Button>
-                        </a>
-                    </div>
-                </div>
-            </div>
-        );
-    }
+	render() {
+		const { classes } = this.props;
+		return (
+			<div className={classes.container}>
+				<div className={classes.content}>
+					<div className={classes.footerItem}>
+						<div className={classes.footerText}>FIND US ON SOCIAL MEDIA</div>
+						<div className={classes.icons}>
+							<Icon icon={facebook} link="https://www.facebook.com/groups/acmhack/"/>
+							<Icon icon={github} link="https://github.com/uclaacm/"/>
+							<Icon icon={medium} link="https://medium.com/techatucla"/>
+							<Icon icon={twitter} link="https://twitter.com/uclaacm"/>
+						</div>
+					</div>
+					<div className={classes.footerItem}>© ACM HACK 2019</div>
+					<div className={classes.footerItem}>
+						<a className={classes.buttonLink} href="http://eepurl.com/c5pE6P">
+							<Button className={classes.button} variant="contained" >Join our Mailing List</Button>
+						</a>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 Footer.propTypes = {
--- src/components/footer/icon.js
+++ src/components/footer/icon.js
@@ -3,26 +3,28 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
 const styles = () => ({
-    icon: {
-        width: '30px',
-    }
+	icon: {
+		width: '30px'
+	}
 });
 
 class Icon extends React.Component {
-    render() {
-        const { classes } = this.props;
-        return (
-            <React.Fragment>
-                <a href={this.props.link}>
-                    <img className={classes.icon} src={this.props.icon} />
-                </a>
-            </React.Fragment>
-        );
-    }
+	render() {
+		const { classes } = this.props;
+		return (
+			<React.Fragment>
+				<a href={this.props.link}>
+					<img className={classes.icon} src={this.props.icon} />
+				</a>
+			</React.Fragment>
+		);
+	}
 }
 
 Icon.propTypes = {
-	classes: PropTypes.object.isRequired
+	classes: PropTypes.object.isRequired,
+	link: PropTypes.string,
+	icon: PropTypes.string
 };
 
 export default withStyles(styles)(Icon);
