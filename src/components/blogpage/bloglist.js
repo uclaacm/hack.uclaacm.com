a809585313583422dc5992d7abe1b492e79d5f78
Jody Lin <jody.m.lin@gmail.com>
2019-08-23 21:19:01 -0700

    spaced out bloglistitem component

--- src/components/blogpage/bloglist.js
+++ src/components/blogpage/bloglist.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { graphql } from 'gatsby';
 import { withStyles } from '@material-ui/core/styles';
 
 import BlogListItem from './bloglistitem';
-import HeadFooter from '../headfooter/headfooter';
 import { Button, Container, List } from '@material-ui/core';
 
 
@@ -15,39 +13,30 @@ const styles = theme => ({
 	},
 	title: {
 		fontFamily: ['Poppins', 'sans-serif'],
-		textAlign: 'center',
+		textAlign: 'left',
 		margin: theme.spacing(2, 0, 0)
 	},
 	buttonRoot: {
-		margin: 'auto'
+		margin: '8px auto' // don't know how to use auto with theme.spacing
 	},
 	link: {
 		textDecoration: 'none'
 	}
 });
 
-class BlogList extends React.Component {
-	render() {
-		const { data, classes } = this.props;
-		const blogListItem = data.allMarkdownRemark.nodes.map(blog => <BlogListItem key={blog.id} nodes={blog} />);
-		blogListItem.splice(3);
-
-		return (
-			<HeadFooter>
-				<Container maxWidth="md" className={classes.container}>
-					<h1 className={classes.title}>Our Latest Posts</h1>
-					<List>
-						{blogListItem}
-					</List>
-					<Button variant="outlined" color="primary" href='../blog' classes={{ root: classes.buttonRoot }}>
-						More Posts
-					</Button>
-				</Container>
-			</HeadFooter>
-
-
-		);
-	}
+function BlogList({ data, classes }) {
+	const blogListItem = data.allMarkdownRemark.nodes.map(blog => <BlogListItem key={blog.id} nodes={blog} />);
+	return (
+		<Container maxWidth="md" className={classes.container}>
+			<h1 className={classes.title}>Our Latest Posts</h1>
+			<List>
+				{blogListItem}
+			</List>
+			<Button variant="outlined" color="primary" href='../blog' classes={{ root: classes.buttonRoot }}>
+				More Posts
+			</Button>
+		</Container>
+	);
 }
 
 BlogList.propTypes = {
@@ -56,25 +45,5 @@ BlogList.propTypes = {
 };
 
 export default withStyles(styles)(BlogList);
-
-export const pageQuery = graphql`
-query BlogListInfo {
-	allMarkdownRemark(sort: {order: DESC, fields: frontmatter___date}, limit: 3) {
-	  nodes {
-		excerpt(pruneLength: 200)
-		timeToRead
-		frontmatter {
-		  date(formatString: "MMMM D, YYYY")
-		  subtitle
-		  title
-		}
-		fields {
-			slug
-		}
-		id
-	  }
-	}
-  }  
-`;
 
 
--- src/components/blogpage/bloglistitem.js
+++ src/components/blogpage/bloglistitem.js
@@ -12,15 +12,25 @@ const styles = theme => ({
 		alignItems: 'flex-start'
 	},
 	heading: {
-		fontFamily: ['Poppins', 'sans-serif'],
-		color: '#000000'
+		color: '#000000',
+		textDecoration: 'none'
 	},
 	title: {
-		fontSize: theme.typography.fontSize * 1.2,
-		fontWeight: 600,
-		lineHeight: 0.8
+		fontFamily: ['Poppins', 'sans-serif'],
+		fontSize: theme.typography.fontSize * 1.3,
+		fontWeight: 600
 	},
-	description: {
+	subtitle: {
+		fontFamily: ['Poppins', 'sans-serif'],
+		fontSize: theme.typography.fontSize * 0.9
+	},
+	excerpt: {
+		fontSize: theme.typography.fontSize * 0.9,
+		fontFamily: theme.typography.fontFamily,
+		color: '#0000008A',
+		padding: theme.spacing(1, 0)
+	},
+	info: {
 		fontSize: theme.typography.fontSize * 0.9,
 		fontFamily: theme.typography.fontFamily,
 		color: '#0000008A'
@@ -30,13 +40,18 @@ const styles = theme => ({
 function BlogListItem({ nodes, classes }) {
 	const { frontmatter, fields } = nodes;
 	return (
-		<ListItem divider component='a' href={fields.slug} className={classes.listItem} >
-			<div className={classes.heading}>
-				<div className={classes.title}>{frontmatter.title}</div>
-				<div>{frontmatter.subtitle}</div>
-			</div>
-			<Typography className={classes.description}>{nodes.excerpt}</Typography>
-			<Typography className={classes.description}>
+		/*
+			TODO:
+				- Only Click Title and Subtitle to go to blog page
+				- Space everything apart to distinguish parts of post
+		*/
+		<ListItem divider disableGutters className={classes.listItem} >
+			<a href={fields.slug} className={classes.heading}>
+				<Typography className={classes.title}>{frontmatter.title}</Typography>
+				<Typography className={classes.subtitle}>{frontmatter.subtitle}</Typography>
+			</a>
+			<Typography className={classes.excerpt}>{nodes.excerpt}</Typography>
+			<Typography className={classes.info}>
 				{frontmatter.date}  | {nodes.timeToRead} min read
 			</Typography>
 		</ListItem>
