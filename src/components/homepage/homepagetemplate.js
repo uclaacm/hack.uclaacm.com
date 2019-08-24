254446c8a3547513d93f8c6c951a2953ab32a474
Jody Lin <jody.m.lin@gmail.com>
2019-08-23 21:19:01 -0700

    spaced out bloglistitem component

--- src/components/homepage/homepagetemplate.js
+++ src/components/homepage/homepagetemplate.js
@@ -1,15 +1,44 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
 import Banner from '../banner/banner';
+import BlogList from '../blogpage/bloglist';
 import HackDescription from '../hackdescription/hackdescription';
 import HeadFooter from '../headfooter/headfooter';
 
-export default class PlayGround extends React.Component {
+export default class HomePage extends React.Component {
 	render() {
+		const { data } = this.props;
 		return (
 			<HeadFooter>
 				<Banner />
 				<HackDescription />
+				<BlogList data={data}/>
 			</HeadFooter>
 		);
 	}
 }
+
+export const pageQuery = graphql`
+query BlogListInfo {
+	allMarkdownRemark(sort: {order: DESC, fields: frontmatter___date}, limit: 3) {
+	  nodes {
+		excerpt(pruneLength: 175)
+		timeToRead
+		frontmatter {
+		  date(formatString: "MMMM D, YYYY")
+		  subtitle
+		  title
+		}
+		fields {
+			slug
+		}
+		id
+	  }
+	}
+  }  
+`;
+
+HomePage.propTypes = {
+	data: PropTypes.object.isRequired
+};
