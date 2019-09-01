1c62ce2ae2dd75c48e1a7d316a0597ae4385e72d
Galen Wong <wonggalen1999@gmail.com>
2019-09-01 10:49:20 +0800

    Wrapper around Gatsby's Link but with no style (#31)
    * Link no style
    
    * Dependencies for LinkNoStyle
    
    * Fix class name overwrite

---
+++ src/components/linknostyle/linknostyle.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'gatsby';
+import { withStyles } from '@material-ui/styles';
+import classNames from 'classnames';
+
+const styles = () => ({
+	link: {
+		textDecoration: 'none'
+	}
+});
+
+function LinkNoStyle({ classes, className, ...props }) {
+	return <Link className={classNames(classes.link, className)} {...props} />;
+}
+
+LinkNoStyle.propTypes = {
+	classes: PropTypes.object.isRequired,
+	className: PropTypes.string
+};
+
+export default withStyles(styles)(LinkNoStyle);
