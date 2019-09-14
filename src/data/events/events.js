f8114d1b9facdd0f183bec13e45e67363e987db9
GalenWong <wonggalen1999@gmail.com>
2019-09-14 13:05:23 +0800

    Fix broken link on event card

--- src/data/events/events.js
+++ src/data/events/events.js
@@ -9,28 +9,28 @@ const events = [
 		date: new Date(2019, 8, 27, 18, 0),
 		location: 'Boelter Hall 5249',
 		imgURL: Imgg,
-		detailLink: '/posts/fall2019/welcome/welcome'
+		detailLink: '/posts/fall2019/welcome'
 	},
 	{
 		name: 'Hackschool Session 2',
 		date: new Date(2019, 9, 3, 18, 0),
 		location: 'Boelter Hall 5249',
-		imgURL: Imgg,
-		detailLink: '/posts/fall2019/welcome/welcome'
+		imgURL: Imgg
+		// detailLink: '/posts/fall2019/welcome'
 	},
 	{
 		name: 'Hack on the Hill',
 		date: new Date(2019, 10, 3, 8, 0),
 		location: 'Carnesale Commons',
-		imgURL: Imgg,
-		detailLink: '/posts/fall2019/welcome/welcome'
+		imgURL: Imgg
+		// detailLink: '/posts/fall2019/welcome'
 	},
 	{
 		name: 'Hackschool Session 3',
 		date: new Date(2019, 9, 14, 19, 0),
 		location: 'Boelter Hall 5249',
-		imgURL: Imgg,
-		detailLink: '/posts/fall2019/welcome/welcome'
+		imgURL: Imgg
+		// detailLink: '/posts/fall2019/welcome'
 	}
 ];
 
