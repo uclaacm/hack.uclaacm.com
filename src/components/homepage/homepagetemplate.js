75463162c8be87032775500d90d4b571f85bdefb
Jody Lin <jody.m.lin@gmail.com>
2019-09-13 20:17:01 -0700

    Jody/blogpage (#27)
    * created basic layout of BlogListItem and BlogList
    
    * adding graphql to bloglist
    
    * FINALLY properly use graphql and render data in bloglist
    
    * update styling and limit # of blogs shown
    
    * added page links to bloglistitems
    
    * updated query to sort by date and limit to 3
    
    * spaced out bloglistitem component
    
    * changed List to Grid, added hover effect to BlogListItem
    
    * changed bloglistitem to use card
    
    * fixed height of bloglistitem and added link component
    
    * created basic layout of BlogListItem and BlogList
    
    * adding graphql to bloglist
    
    * FINALLY properly use graphql and render data in bloglist
    
    * update styling and limit # of blogs shown
    
    * added page links to bloglistitems
    
    * updated query to sort by date and limit to 3
    
    * spaced out bloglistitem component
    
    * changed List to Grid, added hover effect to BlogListItem
    
    * changed bloglistitem to use card
    
    * fixed height of bloglistitem and added link component
    
    * update link with linknostyle
    
    * Fix text-decoration problem with LinkNoStyle
    
    - use inherit for color and textDecoration
    
    * created BlogPage component
    
    * created blogpagelist file
    
    * set up blogpage to display blogs
    
    * added blogpage pages
    
    * set up prev and next navigation buttons
    
    * Rename blog related component directories
    
    * Basic home page styling
    
    * Finish implementation for page flipping
    
    * Finish blog post page
    
    * Fix blog page GraphQL query to sort by date
    
    * Remove unneeded CSS styles
    
    * fixed blogPerPage to 20

--- src/components/homepage/homepagetemplate.js
+++ src/components/homepage/homepagetemplate.js
@@ -1,18 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'gatsby';
+import { withStyles } from '@material-ui/core';
 
 import Banner from '../banner/banner';
+import BlogList from '../homepageblog/bloglist';
 import HackDescription from '../hackdescription/hackdescription';
 import HeadFooter from '../headfooter/headfooter';
 import HomePageEvent from '../homepageevent/homepageevent';
 
-export default class PlayGround extends React.Component {
-	render() {
-		return (
-			<HeadFooter>
-				<Banner />
-				<HackDescription />
-				<HomePageEvent />
-			</HeadFooter>
-		);
+const styles = theme => ({
+	sectionPadding: {
+		margin: theme.spacing(4, 0, 0, 0)
 	}
+});
+
+function HomePage({ data, classes }) {
+	return (
+		<HeadFooter>
+			<Banner />
+			<HackDescription />
+			<div className={classes.sectionPadding}>
+				<BlogList data={data}/>
+			</div>
+			<div className={classes.sectionPadding}>
+				<HomePageEvent />
+			</div>
+		</HeadFooter>
+	);
 }
+
+export const pageQuery = graphql`
+	query BlogListInfo {
+		allMarkdownRemark(sort: {order: DESC, fields: frontmatter___date}, limit: 4) {
+			nodes {
+				excerpt(pruneLength: 120)
+				timeToRead
+				frontmatter {
+					date(formatString: "MMMM D, YYYY")
+					subtitle
+					title
+				}
+				fields {
+					slug
+				}
+				id
+			}
+		}
+  }
+`;
+
+HomePage.propTypes = {
+	data: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(HomePage);
---
+++ src/components/homepageblog/bloglist.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Button, Container, Grid, Typography } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import TalkBubbleIcon from '@material-ui/icons/ModeCommentTwoTone';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForwardIos';
+
+import BlogListItem from './bloglistitem';
+import LinkNoStyle from '../linknostyle/linknostyle';
+
+
+const styles = theme => ({
+	listItem: {
+		overflowWrap: 'break-word'
+	},
+	title: {
+		fontFamily: theme.typography.fontFamily,
+		fontWeight: 'bold',
+		// align icon with text
+		display: 'flex',
+		alignItems: 'center'
+	},
+	titleIcon: {
+		marginRight: theme.spacing(1),
+		fontSize: 'inherit'
+	},
+	forwardArrow: {
+		marginLeft: theme.spacing(1),
+		fontSize: theme.typography.fontSize * 0.75
+	},
+	viewAllBtn: {
+		margin: theme.spacing(2, 0)
+	}
+});
+
+function BlogList({ data, classes }) {
+	const blogListItem = data.allMarkdownRemark.nodes.map(blog =>
+		<Grid item sm={12} md={6} key={blog.id} className={classes.listItem}>
+			<BlogListItem nodes={blog} />
+		</Grid>);
+	return (
+		<Container maxWidth="md">
+			<Typography variant="h4" classes={{ root: classes.title }}>
+				<TalkBubbleIcon color="primary" classes={{ root: classes.titleIcon }} />
+				Blogs
+			</Typography>
+			<LinkNoStyle to='/blog'>
+				<Button variant="outlined" classes={{ root: classes.viewAllBtn }}>
+					View all blogs
+					<ArrowForwardIcon classes={{ root: classes.forwardArrow }} />
+				</Button>
+			</LinkNoStyle>
+			<Grid container spacing={2}>
+				{blogListItem}
+			</Grid>
+		</Container>
+	);
+}
+
+BlogList.propTypes = {
+	data: PropTypes.object.isRequired,
+	classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(BlogList);
+
+
