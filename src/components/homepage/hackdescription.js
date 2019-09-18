7753323bdcb5bd3a9a861a55ebb604d3ee16c739
Jody Lin <jody.m.lin@gmail.com>
2019-09-17 17:27:37 -0700

    fixed margins and changed style

--- src/components/homepage/hackdescription.js
+++ src/components/homepage/hackdescription.js
@@ -1,78 +1,57 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import { Container, Typography } from '@material-ui/core';
 
 import hackLogoURL from '../../images/acm-hack-logo.svg';
 
 const styles = theme => ({
 	container: {
-		backgroundColor: '#ffffff',
-		display: 'flex',
-		justifyContent: 'center',
-		alignItems: 'center',
-		margin: 'auto'
-	},
-	content: {
-		position: 'relative',
+		marginTop: theme.spacing(4),
 		display: 'flex',
 		justifyContent: 'space-between',
 		[theme.breakpoints.down('xs')]: {
 			flexDirection: 'column'
 		},
 		flexDirection: 'row-reverse',
-		alignItems: 'center',
-		margin: '20px auto 20px',
-		padding: '0 20px',
-		maxWidth: '670px'
-	},
-	text: {
-		display: 'inline-block',
-		maxWidth: '77%',
-		[theme.breakpoints.down('xs')]: {
-			maxWidth: '100%',
-			paddingLeft: '20px',
-			paddingRight: '20px'
-		},
-		fontFamily: ['Poppins', 'sans-serif']
-	},
-	title: {
-		fontSize: '32px',
-		textAlign: 'center',
-		fontWeight: 'bold',
-		padding: '6px'
+		alignItems: 'center'
 	},
 	description: {
-		lineHeight: '140%',
-		fontSize: '15px'
+		margin: theme.spacing(2, 0),
+		width: '90%',
+		[theme.breakpoints.down('xs')]: {
+			width: '100%'
+		},
+		textAlign: 'justify'
 	},
-	background: {
-		width: '100%',
-		overflowX: 'hidden',
-		position: 'absolute'
+	title: {
+		fontFamily: theme.typography.fontFamily,
+		fontWeight: 'bold'
 	},
 	logo: {
-		width: 120
+		width: 150
 	}
 });
 
-const hackDescription = 'We are a student-run organization that aims ' +
-'to empower UCLA students to influence their world through code. ' +
-'We host events for coders of all skills levels. So whether you\'ve ' +
-'been to 10 hackathons, or you just learned, "Hello World," we\'re happy to have you.';
+const hackDescription = `We are a student-run organization that aims
+to empower UCLA students to influence their world through code.
+We host events for coders of all skills levels. So whether you've
+been to 10 hackathons, or you just learned, "Hello World," we're happy to have you.`;
 
 class HackDescription extends React.Component {
 	render() {
 		const { classes } = this.props;
 		return (
-			<div className={classes.container}>
-				<div className={classes.content}>
-					<img className={classes.logo} src={hackLogoURL}/>
-					<div className={classes.text}>
-						<div className={classes.title}>What is Hack?</div>
-						<div className={classes.description}>{hackDescription}</div>
+			<Container maxWidth="md" classes={{ root: classes.container }}>
+				<img className={classes.logo} src={hackLogoURL}/>
+				<div>
+					<Typography variant="h4" classes={{ root: classes.title }}>What is Hack?</Typography>
+					<div className={classes.description}>
+						<Typography variant="body1">{hackDescription}</Typography>
 					</div>
 				</div>
-			</div>
+			</Container>
+
 		);
 	}
 }
