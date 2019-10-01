f0fb71f316b092436954c2bc4ba65643f9ac67b5
GalenWong <wonggalen1999@gmail.com>
2019-09-30 21:15:40 -0700

    Fixed upcoming events

--- src/data/events/events.js
+++ src/data/events/events.js
@@ -1,35 +1,35 @@
-import Imgg from './combined.png';
+import HackSchoolWeek2 from './images/week-02.png';
+import HackSchoolWeek3 from './images/week-03.png';
+import JSChat from './images/js-chat.png';
 
 /**
  * Month starts at 0!
  */
 const events = [
 	{
+		name: 'JavaScript Chat with ACM Hack',
+		date: new Date(2019, 9, 8, 16, 0),
+		location: 'Engineering 6 Room 372',
+		imgURL: JSChat
+	},
+	{
 		name: 'Hackschool Session 1',
-		date: new Date(2019, 8, 27, 18, 0),
+		date: new Date(2019, 9, 9, 18, 0),
 		location: 'Boelter Hall 5249',
-		imgURL: Imgg,
-		detailLink: '/posts/fall2019/welcome'
+		imgURL: HackSchoolWeek2
+		// detailLink: '/posts/fall2019/welcome'
+	},
+	{
+		name: 'JavaScript Chat with ACM Hack',
+		date: new Date(2019, 9, 15, 16, 0),
+		location: 'Engineering 6 Room 372',
+		imgURL: JSChat
 	},
 	{
 		name: 'Hackschool Session 2',
-		date: new Date(2019, 9, 3, 18, 0),
+		date: new Date(2019, 9, 16, 18, 0),
 		location: 'Boelter Hall 5249',
-		imgURL: Imgg
-		// detailLink: '/posts/fall2019/welcome'
-	},
-	{
-		name: 'Hack on the Hill',
-		date: new Date(2019, 10, 3, 8, 0),
-		location: 'Carnesale Commons',
-		imgURL: Imgg
-		// detailLink: '/posts/fall2019/welcome'
-	},
-	{
-		name: 'Hackschool Session 3',
-		date: new Date(2019, 9, 14, 19, 0),
-		location: 'Boelter Hall 5249',
-		imgURL: Imgg
+		imgURL: HackSchoolWeek3
 		// detailLink: '/posts/fall2019/welcome'
 	}
 ];
