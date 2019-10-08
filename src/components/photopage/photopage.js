bca907b3fa2458cbdf5063e7ce3f6d8d55bf24c2
Connie <connie90503@gmail.com>
2019-10-07 21:15:33 -0700

    updated bios

--- src/components/photopage/photopage.js
+++ src/components/photopage/photopage.js
@@ -26,73 +26,73 @@ const officers = [
 		role: 'President',
 		name: 'Lea Blum',
 		id: 'lea',
-		description: 'Blurbs coming soon!'
+		description: `Hi there, I’m Lea and I’m one of the Hack Co-Presidents (along with Connie). Come hang out with me and the rest of hack at our events to hear my (sometimes) funny jokes and (always) hilarious puns.`
 	},
 	{
 		role: 'President',
 		name: 'Connie Chen',
 		id: 'connie',
-		description: 'Blurbs coming soon!'
+		description: `Hello! I'm Connie, a third year computer science major. I love the california weather, facebook messenger stickers, hugs, and music. Getting added to the Dogspotting fb group also changed my life (shout-out to my co Lea). I'm super excited to meet you all; come to our events and feel free to chat with us!`
 	},
 	{
 		role: 'Officer',
 		name: 'Shirly Fang',
 		id: 'shirly',
-		description: 'Blurbs coming soon!'
+		description: `Hi! I'm a 3rd year CS major! I enjoy mobile development and I occassionally dabble in Neopets. I also enjoy bunnies and yellowtail.`
 	},
 	{
 		role: 'Officer',
 		name: 'Timothy Gu',
 		id: 'timothyG',
-		description: 'Blurbs coming soon!'
+		description: `Hey there, I'm heading into my third year as a Computer Science student at UCLA, and second year as an officer for the Hack committee of ACM. In the past, I've helped organize some of the most popular ACM events, such as Hackschool (building your first web app) and Hack on the Hill (beginner-friendly hackathon). I will be a co-director for Hackschool this coming quarter. Hope to see you there!`
 	},
 	{
 		role: 'Officer',
 		name: 'Raji Jadhav',
 		id: 'raji',
-		description: 'Blurbs coming soon!'
+		description: `Hey! I'm Raji, a 2nd year Hack officer, originally from India! Love to dance, love to sing, love to code. I believe in community learning, a multiverse, and fairies :) Current obsessions include ML, Thai food, and calligraphy. Tend to never stop talking.`
 	},
 	{
 		role: 'Officer',
 		name: 'Kristie Lim',
 		id: 'kristie',
-		description: 'Blurbs coming soon!'
+		description: `I'm a third year CS major from San Jose, CA. In my free time, I like to run and crochet. Fun fact: I made a Pusheen-themed website to teach high school students about HTML/CSS.`
 	},
 	{
 		role: 'Officer',
 		name: 'Jeanette Lin',
 		id: 'jeanette',
-		description: 'Blurbs coming soon!'
+		description: `Hi, I'm Jeanette, and I'm a fourth year Statistic major. Besides my interest in Data Science, I also like to spend my free time going on hikes, eating with friends,  and binge watching The Office.`
 	},
 	{
 		role: 'Officer',
 		name: 'Jody Lin',
 		id: 'jody',
-		description: 'Blurbs coming soon!'
+		description: `Hi I'm Jody and I'm a second year Computer Science major! I first got into CS in middle school when I wanted to be a hacker at the NSA, but now I'm mostly interested in web and mobile development. When I'm not busy, you can almost always find me on Youtube or watching a TV show.`
 	},
 	{
 		role: 'Officer',
 		name: 'Sahen Rai',
 		id: 'sahen',
-		description: 'Blurbs coming soon!'
+		description: `I'm a board member for ACM Hack. I attend meetings and plan cool events for Hack. In my free time, I like to make lame clarinet covers of pop songs.`
 	},
 	{
 		role: 'Officer',
 		name: 'Tim Rediehs',
 		id: 'timothyR',
-		description: 'Blurbs coming soon!'
+		description: `I'm a board member of Hack and co-director of Hackschool 2019 with Timothy Gu.  Gaining and spreading knowlege is very important to me.  In my free time, I love playing Magic: The Gathering even though I'm bad at it.`
 	},
 	{
 		role: 'Officer',
 		name: 'Furn Techalertumpai',
 		id: 'furn',
-		description: 'Blurbs coming soon!'
+		description: `I’m a fourth-year CS major from Bangkok, Thailand. I recently interned at Facebook in their Service Infra team. In my free time, I like to gym, do yoga, travel, and eat a lot of food!`
 	},
 	{
 		role: 'Officer',
 		name: 'Galen Wong',
 		id: 'galen',
-		description: 'Blurbs coming soon!'
+		description: `I teach Web Dev a lot, feel free to ask me questions on JavaScript and React. I randomly organized a hot pot party at Kevin's place and they seem pretty satisfied.`
 	}
 ];
 /* eslint-enable max-len */
