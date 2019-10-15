ab3d701ee92ba9b7f29f20db53e26727ce074826
GalenWong <wonggalen1999@gmail.com>
2019-10-14 20:28:34 -0700

    Update events data

--- src/data/events/events.js
+++ src/data/events/events.js
@@ -22,13 +22,27 @@ const events = [
 		name: 'JavaScript Chats',
 		date: getDateTime(2019, 10, 15, 16),
 		location: 'Engineering VI 372',
-		imgFilePath: 'event/2019f-js-chat.png'
+		imgFilePath: 'event/2019f-js-chat-2.png',
+		detailLink: 'https://www.facebook.com/events/475926219661264/'
 	},
 	{
 		name: 'Hackschool Session 2',
 		date: getDateTime(2019, 10, 16, 18),
 		location: 'Boelter Hall 5249',
-		imgFilePath: 'event/2019f-hackschool-week-03.png'
+		imgFilePath: 'event/2019f-hackschool-week-03.png',
+		detailLink: 'https://www.facebook.com/events/2427000610907217/'
+	},
+	{
+		name: 'JavaScript Chats',
+		date: getDateTime(2019, 10, 22, 16),
+		location: 'Engineering VI 372',
+		imgFilePath: 'event/2019f-js-chat.png'
+	},
+	{
+		name: 'Hackschool Session 3',
+		date: getDateTime(2019, 10, 23, 6),
+		location: 'Boelter Hall 5249',
+		imgFilePath: 'event/2019f-hackschool.png'
 	}
 ];
 
