9775b70169d12ae0359997b8d5e3c0877c2c2a2a
khxia <55375910+khxia@users.noreply.github.com>
2020-05-17 20:46:24 -0700

    Updated Officer Bios (#71)
    * Updated officer bios
    
    * Fixed formatting issues

--- src/components/PhotoPage/PhotoPage.js
+++ src/components/PhotoPage/PhotoPage.js
@@ -10,110 +10,91 @@ import Grid from '@material-ui/core/Grid';
 import { ReactComponent as HeaderBar } from './head.svg';
 import Profile from './Profile';
 
-const teamIntro = `
-	We are a group of hackers, designers, and engineers all working to improve UCLA's
-	hacking community. We believe in moving fast, having fun, and being passionate
-	about using technology to solve problems that are relevant to us. We have a high
-	bar for success, and are willing to work incredibly hard, balancing school and
-	many other things, to improve the experience of other students around us. We're
-	hiring in the spring, so keep a look out for an upcoming application form.
-`;
+const teamIntro = `We are a group of hackers, designers, and engineers all working to improve UCLA's
+hacking community. We believe in moving fast, having fun, and being passionate
+about using technology to solve problems that are relevant to us. We have a high
+bar for success, and are willing to work incredibly hard, balancing school and
+many other things, to improve the experience of other students around us.`;
 
 /* eslint-disable max-len */
 // The id field must be sync'd with the name of the file.
 const officers = [
 	{
 		role: 'President',
-		name: 'Lea Blum',
-		id: 'lea',
-		description: `
-			Hi there, I’m Lea and I’m one of the Hack Co-Presidents (along with
-			Connie). Come hang out with me and the rest of hack at our events to hear
-			my (sometimes) funny jokes and (always) hilarious puns.
-		`
-	},
-	{
-		role: 'President',
-		name: 'Connie Chen',
-		id: 'connie',
-		description: `Hello! I'm Connie, a third year computer science major. 
-		I love the california weather, facebook messenger stickers, and music. 
-		Getting added to the Dogspotting fb group also changed my life (shout-out to my co Lea).`
-	},
-	{
-		role: 'Officer',
-		name: 'Shirly Fang',
-		id: 'shirly',
-		description: `Hi! I'm a 3rd year CS major! I enjoy mobile development
-		 and I occassionally dabble in Neopets. I also enjoy bunnies and yellowtail.`
-	},
-	{
-		role: 'Officer',
-		name: 'Timothy Gu',
-		id: 'timothyG',
-		description: `Hey there, I'm heading into my third year as a 
-		Computer Science student at UCLA, and second year as an officer 
-		for the Hack committee of ACM. 
-		I will be a co-director for Hackschool this coming quarter. Hope to see you there!`
-	},
-	{
-		role: 'Officer',
 		name: 'Raji Jadhav',
 		id: 'raji',
-		description: `Hey! I'm Raji, a 2nd year Hack officer, originally from 
+		description: `Hey! I'm Raji, a 2nd year from 
 		India! Love to dance, love to sing, love to code. I believe in 
 		community learning, a multiverse, and fairies :) Current obsessions 
 		include ML, Thai food, and calligraphy. Tend to never stop talking.`
 	},
 	{
+		role: 'Vice President',
+		name: 'Jody Lin',
+		id: 'jody',
+		description: `Hi I'm Jody and I'm a 2nd year CS Major! 
+		I love going to karaoke with friends, hanging out with my hack family, and playing guitar! 
+		I also may or may not be addicted to kdramas.`
+	},
+	{
+		role: 'Officer',
+		name: 'Shirly Fang',
+		id: 'shirly',
+		description: `I am a yellowtail enthusiast and bunny mom who loves to develop iOS apps! 
+		Hack is great!`
+	},
+	{
+		role: 'Officer',
+		name: 'Timothy Gu',
+		id: 'timothyG',
+		description: `I'm a third-year Computer Science major with a minor in Linguistics. 
+		I like messing with JavaScript, reading Wikipedia, and teaching Vim to people!`
+	},
+	{
+		role: 'Officer',
+		name: 'Asha Kar',
+		id: 'asha',
+		description: `Hello! I'm a first year CS major with a deep love for coffee and dogs. 
+		Besides coding, I enjoy knitting, reading, bullet journaling, and watching sitcoms.`
+	},
+	{
 		role: 'Officer',
 		name: 'Kristie Lim',
 		id: 'kristie',
-		description: `I'm a third year CS major from San Jose, CA. In my free 
-		time, I like to run and crochet. Fun fact: I made a Pusheen-themed 
-		website to teach high school students about HTML/CSS.`
+		description: `I'm a Computer Science major (class of 2021) from San Jose, California.
+		Over winter quarter, I studied abroad in Singapore! In my free time, I've been crocheting 
+		cacti, turtles, and AirPods cases.`
 	},
 	{
 		role: 'Officer',
-		name: 'Jeanette Lin',
-		id: 'jeanette',
-		description: `Hi, I'm Jeanette, and I'm a fourth year Statistic major. 
-		Besides my interest in Data Science, I also like to spend my free 
-		time going on hikes, eating with friends,  and binge watching The Office.`
+		name: 'Jamie Liu',
+		id: 'jamie',
+		description: `Hey, I'm a third year CS major from Princeton, New Jersey (yay east coast)! 
+		In my spare time I love to cook, bake, and binge watch TV. I like listening to music and I 
+		drink way too much boba (cough cough Miles).`
 	},
 	{
 		role: 'Officer',
-		name: 'Jody Lin',
-		id: 'jody',
-		description: `Hi I'm Jody and I'm a second year CS major! 
-		I wanted to be a hacker at the NSA in middle school, 
-		but now I'm mostly interested in web and mobile development. When I'm not busy, 
-		you can find me playing guitar or watching TV.`
+		name: 'Eugene Lo',
+		id: 'eugene',
+		description: `Hi there! My name is Eugene and I'm a 2nd year computer science major. 
+		I'm currently focused on iOS development. In my free time, you can catch me singing 
+		too loudly, binging Youtube videos, or making too many Spotify playlists.`
 	},
 	{
 		role: 'Officer',
 		name: 'Sahen Rai',
 		id: 'sahen',
-		description: `I'm a board member for ACM Hack. I attend meetings and plan 
-		cool events for Hack. In my free time, I like to make lame clarinet covers 
-		of pop songs.`
+		description: `Hi I'm Sahen! I'm a second year student, and in addition to ACM I also participate in the Foundations dance Org :).
+		In my free time, I also like to make lame clarinet covers of pop songs and watch old spongebob episodes.`
 	},
 	{
 		role: 'Officer',
 		name: 'Tim Rediehs',
 		id: 'timothyR',
 		description: `I'm a board member of Hack and co-director of Hackschool 
-		2019 with Timothy Gu.  Gaining and spreading knowlege is very important 
-		to me.  In my free time, I love playing Magic: The Gathering even though I'm bad at it.`
-	},
-	{
-		role: 'Officer',
-		name: 'Furn Techalertumpai',
-		id: 'furn',
-		description: `Hi! I'm a fourth year CS major from Bangkok, Thailand 
-		(you can tell from my very long last name). I am a BIG fan of food and 
-		I like to listen to new jazz/R&B music. I will be co-directing Hack 
-		Kitchen with Raji this quarter so come say hi!!`
+		2019 with Timothy Gu. Gaining and spreading knowlege is very important 
+		to me. In my free time, I love playing Magic: The Gathering even though I'm bad at it.`
 	},
 	{
 		role: 'Officer',
@@ -122,6 +103,36 @@ const officers = [
 		description: `I teach Web Dev a lot, feel free to ask me questions on 
 		JavaScript and React. I randomly organized a hot pot party at Kevin's 
 		place and they seem pretty satisfied.`
+	},
+	{
+		role: 'Officer',
+		name: 'Miles Wu',
+		id: 'miles',
+		description: `I'm a CS major passionate about hackathons and Powell library.
+		Most of my experience deals with web/mobile dev. Catch me out here eating way too much food, drinking way too 
+		much boba (@Jamie for enabling me), and vibing to chill music.`
+	},
+	{
+		role: 'Officer',
+		name: 'Alex Xia',
+		id: 'alex',
+		description: `Hi, I'm a first year CS major from Hong Kong. Aside from coding, 
+		I like to watch anime and play games – occasionally, Jody baits me into watching kdrama. 
+		When I am not doing these things, I am at the UCLA badminton club.`
+	},
+	{
+		role: 'Advisor',
+		name: 'Lea Blum',
+		id: 'lea',
+		description: `Hi there, I'm Lea. Come hang out with me and the rest of hack at our events (on zoom) to hear my 
+		(sometimes) funny jokes and (always) hilarious puns. Currently living off of Disney movies in quarantine.`
+	},
+	{
+		role: 'Advisor',
+		name: 'Connie Chen',
+		id: 'connie',
+		description: `Hello! My name is Connie, and I'm a third year Computer Science major and Cognitive Science minor. 
+		I like the California weather, Messenger stickers, and music. Currently living off of Disney movies in quarantine.`
 	}
 ];
 /* eslint-enable max-len */
