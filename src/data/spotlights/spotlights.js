81cb30dd4af9db9959f1f8e599a5ec0db1d9f0be
Jody Lin <jody.m.lin@gmail.com>
2020-06-20 21:28:10 -0700

    updated blurb

--- src/data/spotlights/spotlights.js
+++ src/data/spotlights/spotlights.js
@@ -4,11 +4,11 @@ const spotlights = [
 		class: 'Class of 2022',
 		aboutHTML: `Lee Ji-eun (이지은), known professionally 
         as IU (아이유), is a South Korean singer-songwriter and actress. 
-        She signed with Kakao M in 2007 as a 
-        trainee and debuted as a singer at the age of fifteen. 
-        Although her follow-up albums, it was after the release of "Good Day" 
-        (Korean: 좋은 날), the lead single from her 2010 album Real, that she 
-        achieved national stardom. <a href='https://www.youtube.com/watch?v=TgOu00Mf3kI'>eight uwu</a>`,
+        She signed with Kakao M in 2007 as a trainee and debuted as a 
+        singer at the age of fifteen. It was after the release of "Good 
+        Day" (Korean: 좋은 날), the lead single from her 2010 album Real, that she 
+        achieved national stardom. Check out her website 
+        <a href='http://madeedam.com/'> here</a>`,
 		imgFilePath: 'spotlight/placeholder.jpg',
 		button: 'Portfolio',
 		link: 'https://www.youtube.com/watch?v=TgOu00Mf3kI'
