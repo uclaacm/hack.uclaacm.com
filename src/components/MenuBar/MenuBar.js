909fbbcfe7daca69d7b7c5d3ed63bcbc3dc9ee99
GalenWong <wonggalen1999@gmail.com>
2020-07-19 12:03:13 +0800

    Using Drawer to reimplement the mobile bar

--- src/components/MenuBar/MenuBar.js
+++ src/components/MenuBar/MenuBar.js
@@ -1,29 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { navigate } from 'gatsby';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
-import PropTypes from 'prop-types';
-import { Collapse } from '@material-ui/core';
-import { withStyles } from '@material-ui/core/styles';
+import { Drawer, useMediaQuery } from '@material-ui/core';
 import MenuIcon from '@material-ui/icons/Menu';
 
 import ButtonBar from './ButtonBar';
 import { ReactComponent as HackLogo } from '../../images/acm-hack-logo.svg';
 import { ReactComponent as ACMHackWordmark } from '../../images/acm-hack-wordmark.svg';
+import { makeStyles, useTheme } from '@material-ui/styles';
 
-const styles = theme => {
-	/**
-	 * This is the limit of the screensize where the MenuBar
-	 * should switch between desktop and mobile.
-	 */
-	const menuBarAdaptiveThreshold = theme.breakpoints.values.sm * 1.05;
+/**
+ * This is the limit of the screensize where the MenuBar
+ * should switch between desktop and mobile.
+ */
+const mobileAdaptiveThreshold = theme => theme.breakpoints.values.sm;
+
+const useStyles = makeStyles(theme => {
+	const menuBarAdaptiveThreshold = mobileAdaptiveThreshold(theme);
 	return {
 		logohome: {
 			display: 'flex',
 			alignItems: 'center',
 			justifyContent: 'center',
 			cursor: 'pointer'
+		},
+		appbar: {
+			zIndex: theme.zIndex.modal + 1 // overlap on mobile drawer
 		},
 		toolbar: {
 			backgroundColor: 'white',
@@ -63,26 +67,38 @@ const styles = theme => {
 			// default styling
 			boxShadow: theme.shadows[4],
 			position: 'fixed',
-			zIndex: '1',
 			overflow: 'hidden',
 			backgroundColor: theme.palette.grey[100],
 			width: '100%'
 		},
 		mobileBtnContainer: {
-			display: 'flex',
-			flexDirection: 'column',
-			padding: '3%'
+			top: theme.spacing(7),
+			padding: theme.spacing(1)
 		}
 	};
-};
+});
 
-function MenuBar({ classes }) {
+function MenuBar() {
+	const classes = useStyles();
+
+	// only control state in mobile
 	const [menuOpen, setMenuOpen] = useState(false);
 	const toggleMenu = () => setMenuOpen(o => !o);
 
+	const theme = useTheme();
+	const threshold = mobileAdaptiveThreshold(theme);
+	const isMobile = useMediaQuery(theme.breakpoints.down(threshold));
+
+	// if we got out of mobile, we force the menu to close
+	useEffect(() => {
+		if (!isMobile) {
+			setMenuOpen(false);
+		}
+	}, [isMobile]);
+
 	return (
 		<React.Fragment>
-			<AppBar position="sticky">
+			<AppBar position="sticky" classes={{ root: classes.appbar }}>
 				<Toolbar className={classes.toolbar}>
 					<div
 						className={classes.logohome}
@@ -99,24 +115,28 @@ function MenuBar({ classes }) {
 					<IconButton onClick={toggleMenu} className={classes.menubtn}>
 						<MenuIcon/>
 					</IconButton>
+
+					{/* Mobile menu Bar */}
+					<Drawer
+						anchor="top"
+						open={menuOpen}
+						onClose={() => setMenuOpen(false)}
+						ModalProps={{ disableScrollLock: true }}
+						PaperProps={{
+							classes: { root: classes.mobileBtnContainer }
+						}}
+						transitionDuration={{
+							enter: !isMobile ? 0 : theme.transitions.duration.enteringScreen,
+							exit: !isMobile ? 0 : theme.transitions.duration.leavingScreen
+						}}
+					>
+						<ButtonBar isMobile/>
+					</Drawer>
+
 				</Toolbar>
 			</AppBar>
-
-			{/* Mobile menu Bar */}
-			<div className={classes.mobileMenuBar}>
-				<Collapse in={menuOpen}>
-					<div className={classes.mobileBtnContainer}>
-						<ButtonBar isMobile/>
-					</div>
-				</Collapse>
-			</div>
-
 		</React.Fragment>
 	);
 }
 
-MenuBar.propTypes = {
-	classes: PropTypes.object.isRequired
-};
-
-export default withStyles(styles)(MenuBar);
+export default MenuBar;
