76b6e6211d499718b4b4a27da4ce5dee72a92972
GalenWong <wonggalen1999@gmail.com>
2020-09-27 12:03:32 +0800

    adds easter egg feature

--- src/components/PhotoPage/Cropper.js
+++ src/components/PhotoPage/Cropper.js
@@ -1,33 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Img from 'gatsby-image';
 
 // This cropper only crops landscape-oriented/square image.
-const styles = {
+const useStyles = makeStyles({
 	cropper: {
 		borderRadius: '50%',
 		overflow: 'hidden',
 		display: 'flex',
 		justifyContent: 'center'
 	}
-};
+});
 
 /**
- * Cropper takes an imageFixed object for use with gatsby-image, and crops it
- * to a circle. The diameter of the circle is controlled by the image.
+ * Cropper takes an imageFixed object for use with gatsby-image, and crops it to
+ * a circle. The diameter of the circle is controlled by the image. The
+ * easterEggImageFixed is an imageFixed object that is displayed when the user
+ * clicks on the image. As the name implies, it is an easter egg.
  */
-function Cropper({ classes, imageFixed }) {
+function Cropper({ imageFixed, easterEggImageFixed }) {
+	const classes = useStyles();
+	const [showEasterEgg, setShowEasterEgg] = useState(false);
+
 	return (
-		<div className={classes.cropper}>
-			<Img fixed={imageFixed} />
+		<div className={classes.cropper} onClick={() => setShowEasterEgg(prev => !prev)}>
+			<Img fixed={imageFixed} style={{ display: showEasterEgg ? 'none' : null }} />
+			<Img fixed={easterEggImageFixed} style={{ display: showEasterEgg ? null : 'none' }} />
 		</div>
 	);
 }
 
 Cropper.propTypes = {
-	classes: PropTypes.object.isRequired,
-	imageFixed: PropTypes.object.isRequired
+	imageFixed: PropTypes.object.isRequired,
+	easterEggImageFixed: PropTypes.object.isRequired
 };
 
-export default withStyles(styles)(Cropper);
+export default Cropper;
