eb9f11aa8948bd6ec3467a8f9a3c26ce4a76fa23
Jody Lin <jody.m.lin@gmail.com>
2020-10-02 13:28:56 -0700

    change to use makeStyles hook in PageTitle component

--- src/components/PageTitle/PageTitle.js
+++ src/components/PageTitle/PageTitle.js
@@ -2,25 +2,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
 import classNames from 'classnames';
-import { withStyles } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core';
 
-const styles = theme => ({
+const useStyles = makeStyles(theme => ({
 	headline: {
-		// fontFamily: theme.typography.fontFamily,
 		fontWeight: 600,
 		margin: theme.spacing(6, 0)
 	}
-});
+}));
 
-function PageTitle({ classes, className, ...props }) {
+function PageTitle({ className, ...props }) {
+	const classes = useStyles();
 	return <Typography variant="h2" component='h1' className={classNames(classes.headline, className)} {...props} />;
 }
 
 PageTitle.propTypes = {
-	classes: PropTypes.object.isRequired,
-	className: PropTypes.string,
-	children: PropTypes.object // fix this
+	className: PropTypes.string
 };
-export default withStyles(styles)(PageTitle);
+
+export default PageTitle;
 
 
