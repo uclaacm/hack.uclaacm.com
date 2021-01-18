2178a3b2d8d2274d1119fec3c13427addeb92ba4
Einar Balan <40647794+EinarBalan@users.noreply.github.com>
2021-01-17 21:23:13 -0800

    Update src/components/ArchivePage/EventInfoItem.js
    Co-authored-by: Timothy Gu <timothygu99@gmail.com>

--- src/components/ArchivePage/EventInfoItem.js
+++ src/components/ArchivePage/EventInfoItem.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 /* eslint-disable no-duplicate-imports */
