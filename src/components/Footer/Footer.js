61912cfac06fadca8da1944a3f61d031c833d6ea
EinarBalan <einar.balan@gmail.com>
2021-01-23 19:33:41 -0800

    Added instagram icon to footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,6 +6,7 @@ import Grid from '@material-ui/core/Grid';
 
 import IconLink from './IconLink';
 import { ReactComponent as FacebookIcon } from './facebook.svg';
+import { ReactComponent as InstagramIcon } from './instagram.svg';
 import { ReactComponent as GitHubIcon } from './github.svg';
 import { ReactComponent as MediumIcon } from './medium.svg';
 import { ReactComponent as TwitterIcon } from './twitter.svg';
@@ -53,6 +54,7 @@ function Footer({ classes }) {
 					<Grid item xs={12} sm={12} md={4}>
 						<div className={classes.footerText}>Find us on social media</div>
 						<div className={classes.icons}>
+							<IconLink icon={InstagramIcon} link="https://www.instagram.com/uclahack/"/>
 							<IconLink icon={FacebookIcon} link="https://www.facebook.com/groups/acmhack/"/>
 							<IconLink icon={GitHubIcon} link="https://github.com/uclaacm/"/>
 							<IconLink icon={MediumIcon} link="https://medium.com/techatucla"/>
