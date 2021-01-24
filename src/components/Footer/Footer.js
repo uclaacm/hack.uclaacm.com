02d03cc67cacce95c50796acc083903488807339
Einar Balan <40647794+EinarBalan@users.noreply.github.com>
2021-01-23 20:04:20 -0800

    Updated social media links in footer (#155)
    * Added instagram icon to footer
    
    * Add insta svg
    
    * Removed twitter svg

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,9 @@ import Grid from '@material-ui/core/Grid';
 
 import IconLink from './IconLink';
 import { ReactComponent as FacebookIcon } from './facebook.svg';
+import { ReactComponent as InstagramIcon } from './instagram.svg';
 import { ReactComponent as GitHubIcon } from './github.svg';
 import { ReactComponent as MediumIcon } from './medium.svg';
-import { ReactComponent as TwitterIcon } from './twitter.svg';
 import JoinMailingListBtn from '../JoinMailingListBtn/JoinMailingListBtn';
 
 const styles = theme => ({
@@ -53,10 +53,10 @@ function Footer({ classes }) {
 					<Grid item xs={12} sm={12} md={4}>
 						<div className={classes.footerText}>Find us on social media</div>
 						<div className={classes.icons}>
+							<IconLink icon={InstagramIcon} link="https://www.instagram.com/uclahack/"/>
 							<IconLink icon={FacebookIcon} link="https://www.facebook.com/groups/acmhack/"/>
 							<IconLink icon={GitHubIcon} link="https://github.com/uclaacm/"/>
 							<IconLink icon={MediumIcon} link="https://medium.com/techatucla"/>
-							<IconLink icon={TwitterIcon} link="https://twitter.com/uclaacm"/>
 						</div>
 					</Grid>
 					<Grid item xs={12} sm={12} md={4}>
