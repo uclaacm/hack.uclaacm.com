c9c7000826e33e2119ce7cc231b8f50929d36c5f
GalenWong <wonggalen1999@gmail.com>
2021-03-07 11:58:32 +0800

    change h5 to p for better semantics

--- src/components/EmptyEventMessage/EmptyEventMessage.js
+++ src/components/EmptyEventMessage/EmptyEventMessage.js
@@ -5,13 +5,13 @@ import Typography from '@material-ui/core/Typography';
 function EmptyEventMessage() {
 	return (
 		<Container style={{ paddingTop: '5vh', paddingBottom: '5vh' }}>
-			<Typography variant="h5" align="center" color="textSecondary" gutterBottom>
+			<Typography variant="h5" component="p" align="center" color="textSecondary" gutterBottom>
 				{'💜 Stay tuned for future events'}
 			</Typography>
-			<Typography variant="h5" align="center" color="textSecondary" gutterBottom>
+			<Typography variant="h5" component="p" align="center" color="textSecondary" gutterBottom>
 				{'🚀 We look forward to seeing you'}
 			</Typography>
-			<Typography variant="h5" align="center" color="textSecondary" gutterBottom>
+			<Typography variant="h5" component="p" align="center" color="textSecondary" gutterBottom>
 				{'🙌 And we can\'t wait to see the amazing things YOU build'}
 			</Typography>
 		</Container>
