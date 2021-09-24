5dd466dd02bdeb328b0cf8d1ac902894f9499345
khxia <17xiak124268@gmail.com>
2021-09-23 20:36:45 -0700

    changed banner

--- src/data/events/highlights.js
+++ src/data/events/highlights.js
@@ -1,5 +1,5 @@
 const hackschoolDescription = `
-	Come out and learn how to make websites in our beginner-friendly Fall 
+	Come out and learn how to make websites in our beginner-friendly Fall
 	Hackschool series! We will be teaching the basics of web development
 	with React.js, a popular framework of Javascript. No previous experience
 	is necessary!
@@ -8,15 +8,15 @@ const hackschoolDescription = `
 const passionTalksDescription = `
 	Passion Talks is all about inspiring individuals to dig deeper into the
 	world of tech through empowerment and guided exploration. This is a speaker
-	series led by students who will share their passion for specific fields, 
-	topics, or technologies in the hopes of helping others navigate the vast 
+	series led by students who will share their passion for specific fields,
+	topics, or technologies in the hopes of helping others navigate the vast
 	world of tech to find their own passions.
 `;
 
 const hackFamDescription = `
-	After over a year of quarantine, we are all ready to meet people and 
+	After over a year of quarantine, we are all ready to meet people and
 	make new friends. Hack Fam is all about forming those close connections
-	with new friends. We will be pairing students into families, which will 
+	with new friends. We will be pairing students into families, which will
 	be led by a family head, so you can make lifelong memories and friends!
 `;
 
@@ -32,7 +32,7 @@ const highlightedEvents = [
 	{
 		name: 'Passion Talks',
 		description: passionTalksDescription,
-		imgFilePath: 'event/2021f-passion-talks-temporary.png',
+		imgFilePath: 'event/2021f-passion-talks.png',
 		button: 'Coming Soon'
 	},
 	{
