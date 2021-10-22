cc181e15145e9e1a1c5733e1c1d81260a8d604c9
Jody Lin <jody.m.lin@gmail.com>
2021-10-21 18:52:49 -0700

    update event links

--- src/data/events/events.js
+++ src/data/events/events.js
@@ -3,8 +3,6 @@ function getDateTime(year, month = 1, day = 1, hour = 0, minute = 0, second = 0)
 	return new Date(year, month - 1, day, hour, minute, second);
 }
 
-// ACM GM, Hack gm, hackschool 1, passion talks, hack fam head apps, hack fam members apps
-
 const events = [
 	{
 		name: 'ACM Fall GM',
@@ -23,43 +21,50 @@ const events = [
 		name: 'Hackschool #1: Intro to HTML/CSS',
 		date: getDateTime(2021, 10, 6, 19),
 		location: 'Perloff 1102',
-		imgFilePath: 'event/2021f-hackschool.png'
+		imgFilePath: 'event/2021f-hackschool.png',
+		detailLink: 'https://fb.me/e/4Qj7XuuOV'
 	},
 	{
 		name: 'Hackschool #2: Intro to Javascript',
 		date: getDateTime(2021, 10, 13, 19),
 		location: 'Perloff 1102',
-		imgFilePath: 'event/2021f-hackschool.png'
+		imgFilePath: 'event/2021f-hackschool.png',
+		detailLink: 'https://fb.me/e/cqKQ0nOIz'
 	},
 	{
 		name: 'Hackschool #3: Intro to React',
 		date: getDateTime(2021, 10, 20, 19),
 		location: 'Perloff 1102',
-		imgFilePath: 'event/2021f-hackschool.png'
+		imgFilePath: 'event/2021f-hackschool.png',
+		detailLink: 'https://fb.me/e/15dcLADXc'
 	},
 	{
 		name: 'Hackschool #4: Props and Layout',
 		date: getDateTime(2021, 10, 27, 19),
 		location: 'Perloff 1102',
-		imgFilePath: 'event/2021f-hackschool.png'
+		imgFilePath: 'event/2021f-hackschool.png',
+		detailLink: 'https://fb.me/e/4gsUMa4H6'
 	},
 	{
-		name: 'Hackschool #5: State',
+		name: 'Hackschool #5: Events and State',
 		date: getDateTime(2021, 11, 3, 19),
 		location: 'Perloff 1102',
-		imgFilePath: 'event/2021f-hackschool.png'
+		imgFilePath: 'event/2021f-hackschool.png',
+		detailLink: 'https://fb.me/e/17QRdfe0k'
 	},
 	{
 		name: 'Hackschool #6: Async & Web API',
 		date: getDateTime(2021, 11, 10, 19),
 		location: 'Perloff 1102',
-		imgFilePath: 'event/2021f-hackschool.png'
+		imgFilePath: 'event/2021f-hackschool.png',
+		detailLink: 'https://fb.me/e/47qutYsZT'
 	},
 	{
 		name: 'Hackschool #7: Lifecycle',
 		date: getDateTime(2021, 11, 17, 19),
 		location: 'Perloff 1102',
-		imgFilePath: 'event/2021f-hackschool.png'
+		imgFilePath: 'event/2021f-hackschool.png',
+		detailLink: 'https://fb.me/e/1lo5bLRD2'
 	},
 	// {
 	// 	name: 'Hackschool #8: TBD',
@@ -68,7 +73,7 @@ const events = [
 	// 	imgFilePath: 'event/2021f-hackschool.png'
 	// },
 	{
-		name: 'Passion Talks #1',
+		name: 'Passion Talks #1: Blockchain',
 		date: getDateTime(2021, 10, 21, 18),
 		location: 'Dodd Hall Room 121',
 		imgFilePath: 'event/2021f-passion-talks.png',
@@ -77,16 +82,21 @@ const events = [
 	{
 		name: 'Hack Fam Head Apps Due',
 		date: getDateTime(2021, 9, 25, 0),
-		// location: 'Court of Sciences',
 		imgFilePath: 'event/2020f-hack-fam-banner.jpg',
 		detailLink: 'https://docs.google.com/forms/d/e/1FAIpQLSfTTfhOqDnd8XUAFvoCanP2ooLhxA6cs99AHktUr3fOhcEbMg/viewform'
 	},
 	{
 		name: 'Hack Fam Member Apps Due',
 		date: getDateTime(2021, 10, 7, 0),
-		// location: 'Court of Sciences',
 		imgFilePath: 'event/2020f-hack-fam-banner.jpg',
 		detailLink: 'https://docs.google.com/forms/d/e/1FAIpQLSf3yv_JtF2sIDDddEreT33C7oVFhCSGB4l8oOGySLTqcwxzbQ/viewform'
+	},
+	{
+		name: 'ACM Intern Apps Due',
+		date: getDateTime(2021, 10, 29, 23, 59),
+		imgFilePath: 'event/2021f-intern-app-banner.png',
+		detailLink: 'https://forms.gle/nGEPFjSRzvNaciwC6'
+
 	}
 ];
 
