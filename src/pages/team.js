3c16c3cf7c453cd5092a21f7c0a305bf45c643b2
nareha <nareh.agazaryan@yahoo.com>
2021-10-21 19:14:35 -0700

    Added Alumni section to Team page

--- src/pages/team.js
+++ src/pages/team.js
@@ -3,12 +3,14 @@ import React from 'react';
 import HeadFooter from '../components/HeadFooter/HeadFooter';
 import PhotoPage from '../components/PhotoPage/PhotoPage';
 import SEO from '../components/SEO';
+import Alumni from '../data/events/alumni.js';
 
 function Team() {
 	return (
 		<HeadFooter>
 			<SEO title='Team' />
 			<PhotoPage />
+			<Alumni />
 		</HeadFooter>
 	);
 }
