d295e63f8ee1b223bb367aaee4a8b84fd3d0cda8
nareh agazaryan <53708656+nareha@users.noreply.github.com>
2021-10-23 12:59:38 -0700

    Added Alumni section to Team page (#262)
    * Added Alumni section to Team page
    
    * Updates to Alumni section based on review
    
    * Update src/data/events/alumni.js
    
    Co-authored-by: Jody Lin <jody.m.lin@gmail.com>
    
    * Added spacing between bottom of container and footer
    
    * Changed margin value to MUI spacing values
    
    Co-authored-by: Jody Lin <jody.m.lin@gmail.com>

---
+++ src/data/events/alumni.js
@@ -0,0 +1,87 @@
+import React from 'react';
+
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { Container, Typography, Grid } from '@material-ui/core';
+
+const alumni = {
+	2021: ['Lea Blum', 'Connie Chen', 'Shirly Fang', 'Timothy Gu', 'Kristie Lim', 'Tim Rediehs', 'Galen Wong'],
+	2020: ['Yvonne Chen', 'Jeanette Lin', 'Dustin Newman', 'Prateek Singh', 'Kevin Tan', 'Furn Techalertumpai'],
+	2019: ['Dmitri Brereton', 'Char McGinn', 'Astrid Wang', 'Nathan Yang'],
+	2018: ['Shashank Khanna', 'Breanna Nery', 'Shannon Phu', 'Kelvin Wong'],
+	2017: ['Vic Yeh']
+};
+
+const alumniGradYears = Object.keys(alumni).sort((a, b) => b - a);
+
+const description = `Hack wouldn't be what it is today without our awesome alumni! Even though 
+					these officers have graduated, they will always be part of the Hack family.`;
+
+const styles = theme => ({
+	title: {
+		fontFamily: ['Poppins', 'sans-serif'],
+		fontWeight: 700,
+		fontSize: theme.typography.fontSize * 3,
+		borderStyle: 'solid',
+		borderWidth: `0 0 ${theme.typography.fontSize * 1.5}px 0`,
+		borderColor: theme.palette.secondary.main,
+		width: 'fit-content',
+		margin: theme.spacing(4, 'auto'),
+		[theme.breakpoints.down('xs')]: {
+			fontSize: theme.typography.fontSize * 1.2,
+			borderWidth: `0 0 ${theme.typography.fontSize * 0.6}px 0`
+		}
+	},
+	yearContainer: {
+		margin: theme.spacing(3, 0)
+	},
+	container: {
+		display: 'flex',
+		justifyContent: 'center',
+		marginBottom: theme.spacing(4)
+	},
+	caption: {
+		fontFamily: theme.typography.body1.fontFamily,
+		fontSize: theme.typography.fontSize * 1.3,
+		lineHeight: 1.76,
+		[theme.breakpoints.down('xs')]: {
+			fontSize: theme.typography.fontSize,
+			lineHeight: 1.57
+		}
+	}
+});
+
+function Alumni({ classes }) {
+	const list = alumniGradYears.map(year =>
+		<Grid item className={classes.yearContainer} key={year} sm={12} lg={4}>
+			<Typography variant="h4" align="center">
+				{year}
+			</Typography>
+			<Typography variant="body"> {
+				alumni[year].map(alumniNames =>
+					<Typography variant="body1" align="center" key={alumniNames}>
+						{alumniNames}
+					</Typography>)
+			}
+			</Typography>
+		</Grid>);
+
+	return (
+		<Container maxWidth='md' className={classes.caption}>
+			<Typography variant="h3" component="h2" align="center" className={classes.title}>
+				Alumni
+			</Typography>
+			<Typography variant="body1" align="center" className={classes.caption}>
+				{description}
+			</Typography>
+			<Grid container className={classes.container}>
+				{list}
+			</Grid>
+		</Container>
+	);
+}
+
+Alumni.propTypes = {
+	classes: PropTypes.object.isRequired
+};
+export default withStyles(styles)(Alumni);
--- src/pages/about.js
+++ src/pages/about.js
@@ -3,12 +3,14 @@ import React from 'react';
 import HeadFooter from '../components/HeadFooter/HeadFooter';
 import PhotoPage from '../components/PhotoPage/PhotoPage';
 import SEO from '../components/SEO';
+import Alumni from '../data/events/alumni.js';
 
 function About() {
 	return (
 		<HeadFooter>
 			<SEO title='About' />
 			<PhotoPage />
+			<Alumni />
 		</HeadFooter>
 	);
 }
