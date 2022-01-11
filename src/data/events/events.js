e134b35e478d642a2030f4218c269814c082fb14
khxia <17xiak124268@gmail.com>
2022-01-10 21:59:42 -0800

    added first three hacksprint sessions

--- src/data/events/events.js
+++ src/data/events/events.js
@@ -11,6 +11,30 @@ const events = [
 		imgFilePath: 'event/2022w-acm-winter-gm.jpg',
 		rsvpLink: 'http://links.uclaacm.com/wintergm-rsvp-22',
 		detailLink: 'https://fb.me/e/4QIpqlopc'
+	},
+	{
+		name: 'Hack Sprint #1: Intro to Swift',
+		date: getDateTime(2022, 01, 12, 18),
+		location: 'Zoom',
+		imgFilePath: 'event/2022w-hacksprint-banner.png',
+		conferenceLink: 'https://ucla.zoom.us/j/93761587241?pwd=aWlsV3FxRzl6clVhRDQ0RHF4dmN6dz09',
+		detailLink: 'https://fb.me/e/1AamI7NeY'
+	},
+	{
+		name: 'Hack Sprint #2: Swift Fundamentals',
+		date: getDateTime(2022, 01, 19, 18),
+		location: 'Zoom',
+		imgFilePath: 'event/2022w-hacksprint-banner.png',
+		conferenceLink: 'https://ucla.zoom.us/j/93761587241?pwd=aWlsV3FxRzl6clVhRDQ0RHF4dmN6dz09',
+		// detailLink: 'https://fb.me/e/1AamI7NeY'
+	},
+	{
+		name: 'Hack Sprint #3: State and Bindings',
+		date: getDateTime(2022, 01, 26, 18),
+		location: 'Zoom',
+		imgFilePath: 'event/2022w-hacksprint-banner.png',
+		conferenceLink: 'https://ucla.zoom.us/j/93761587241?pwd=aWlsV3FxRzl6clVhRDQ0RHF4dmN6dz09',
+		// detailLink: 'https://fb.me/e/1AamI7NeY'
 	}
 ];
 
