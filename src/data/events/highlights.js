0e96fd78291ce7cc066651a41c2fbc0b7015727e
nareh agazaryan <53708656+nareha@users.noreply.github.com>
2022-04-04 14:59:04 -0700

    Updated Spring 2022 events (#296)

--- src/data/events/highlights.js
+++ src/data/events/highlights.js
@@ -1,18 +1,20 @@
-const hackSprintDescription = `
-	Come out to ACM Hack's Hack Sprint, a beginner-friendly mobile
-	development workshop series! We'll be covering everything in
-	Swift from building your app from scratch to publishing a fully
-	functional app on the App Store. Our series will end with a
-	chance for each of you to create your own apps while guided by
-	our mentors.
+const hackspaceDescription = `
+	HackSpace is ACM Hack's beginner-friendly, group-project-based
+	series! We'll help you take an app idea and turn it into reality
+	step-by-step with dedicated mentorship, work sessions, and mini-workshops
+	on project management and technical skills. By the end of this,
+	you will have built a project that you can be proud of (and will
+	look like a star on your resume).
 `;
 
-const hothDescription = `
-	Hack on the Hill (HOTH 9) is our annual hackathon.
-	This beginner-friendly hackathon is designed to give you a
-	glimpse of a real hackathon. There will be food, workshops,
-	mentors, and fun prizes for the best hacks! Check out the
-	HOTH website for more info!
+const httDescription = `
+	Hot Tech Tour is ACM Hack's new workshop/competition series!
+	Throughout the quarter, we'll introduce several technologies,
+	and then let you compete in our fast paced competition sessions!
+	If you're interested in cool technologies that go beyond what you'll
+	learn in class and challenging puzzles that will test your new
+	knowledge, then come on out to Hot Tech Tour Wednesdays 6-8 PM
+	in Boelter 9436.
 `;
 
 
@@ -20,18 +22,18 @@ const hothDescription = `
 
 const highlightedEvents = [
 	{
-		name: 'Hack Sprint',
-		description: hackSprintDescription,
-		imgFilePath: 'event/2022w-hacksprint-banner.png',
+		name: 'HackSpace',
+		description: hackspaceDescription,
+		imgFilePath: 'event/2022s-hackspace-banner.png',
 		button: 'Discord',
 		link: 'https://discord.gg/rup2p6fxA5'
 	},
 	{
-		name: 'Hack on the Hill 9',
-		description: hothDescription,
-		imgFilePath: 'event/2022w-hoth9-banner.png',
-		button: 'More Info',
-		link: 'https://hoth.uclaacm.com/'
+		name: 'Hot Tech Tour',
+		description: httDescription,
+		imgFilePath: 'event/2022s-htt-banner.png',
+		button: 'Discord',
+		link: 'https://discord.gg/rup2p6fxA5'
 	}
 ];
 
