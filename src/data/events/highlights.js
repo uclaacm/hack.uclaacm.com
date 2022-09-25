07335913850c142aa998aeadf8a55c05a1bdf658
Katelyn <ydatabox@gmail.com>
2022-09-25 12:31:43 -0700

    add new events and links. some links and locations disabled until they are finalized later this quarter

--- src/data/events/highlights.js
+++ src/data/events/highlights.js
@@ -1,39 +1,50 @@
-const hackspaceDescription = `
-	HackSpace is ACM Hack's beginner-friendly, group-project-based
-	series! We'll help you take an app idea and turn it into reality
-	step-by-step with dedicated mentorship, work sessions, and mini-workshops
-	on project management and technical skills. By the end of this,
-	you will have built a project that you can be proud of (and will
-	look like a star on your resume).
+const hackschoolDescription = `
+	Come out and learn how to make websites in our beginner-friendly Fall
+	Hackschool series! We will be teaching the basics of web development
+	with React.js, a popular framework of Javascript. No previous experience
+	is necessary!
 `;
 
-const httDescription = `
-	Hot Tech Tour is ACM Hack's new workshop/competition series!
-	Throughout the quarter, we'll introduce several technologies,
-	and then let you compete in our fast paced competition sessions!
-	If you're interested in cool technologies that go beyond what you'll
-	learn in class and challenging puzzles that will test your new
-	knowledge, then come on out to Hot Tech Tour Wednesdays 6-8 PM
-	in Boelter 9436.
+const hackKitchenDescription = `
+	Hack Kitchen is a beginner-friendly coding puzzle competition that encourages
+	creativity in technology. Participants will be immersed in a full day of
+	cooking-show themed puzzles, curveball surprises, and fan-favorite Gordon Ramsay
+	references. Will you survive a dinner service from Kitchen Nightmares, become
+	the next MasterChef, or have your dish on the Chopping block?
 `;
 
+const bruinOdysseyDescription = `
+	Bruin Odyssey is a quarter-long puzzle challenge hosted by ACM Hack and ACM Cyber.
+	Every week from week 3 to week 9, we will be releasing a small set of puzzles
+	for participants to solve. You don't need a computer science background to participate
+	- just have fun! So flex your problem solving skills!
+	Learn a little more about computer science! Challenge your friends!  There will even be
+	prizes for the winners!
+`;
 
 // TODO: Fix links & button names on highlightedEvents
 
 const highlightedEvents = [
 	{
-		name: 'HackSpace',
-		description: hackspaceDescription,
-		imgFilePath: 'event/2022s-hackspace-banner.png',
+		name: 'Hackschool',
+		description: hackschoolDescription,
+		imgFilePath: 'event/2022f-hackschool-banner.png',
 		button: 'Discord',
-		link: 'https://discord.gg/rup2p6fxA5'
+		link: 'https://discord.gg/rup2p6fxA5' // TODO: change to fb link if necessary
 	},
 	{
-		name: 'Hot Tech Tour',
-		description: httDescription,
-		imgFilePath: 'event/2022s-htt-banner.png',
-		button: 'Discord',
-		link: 'https://discord.gg/rup2p6fxA5'
+		name: 'Hack Kitchen',
+		description: hackKitchenDescription,
+		imgFilePath: 'event/2022f-hack-kitchen-banner.png',
+		button: 'Sign Up'
+		//link: '' TODO: change Hack Kitchen link
+	},
+	{
+		name: 'Bruin Odyssey',
+		description: bruinOdysseyDescription,
+		imgFilePath: 'event/2022f-bruin-odyssey-banner.png',
+		button: 'Sign Up'
+		//link: '' TODO: change Bruin Odyssey link
 	}
 ];
 
