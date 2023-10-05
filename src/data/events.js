61ca9fd8844dd31f938cd96abd54f2c978e40afa
katelynsyu <41168140+katelynsyu@users.noreply.github.com>
2023-10-04 20:02:01 -0700

    katelyn/updateFall2023Events (#410)
    * add event highlights and events, tbd on some hackfam times and locations
    
    * update HackFam Date, Location, Time for events

--- src/data/events.js
+++ src/data/events.js
@@ -11,54 +11,96 @@ function getDateTime(
 }
 
 const events = [
-	// {
-	// 	name: 'MiniHack',
-	// 	date: getDateTime(2023, 4, 20, 9, 30),
-	// 	location: 'Carnesale Commons Palisades Room',
-	// 	imgFilePath: 'event/hack-banner.png',
-	// 	detailLink: 'https://hoth.uclaacm.com/',
-	// 	rsvpLink: 'https://forms.gle/n7Do1oZv7noBYqt18'
-	// },
 	{
-		name: 'HackCloud #1: Intro and Storage',
-		date: getDateTime(2023, 4, 20, 19, 30),
-		location: 'Engineering VI 289',
-		imgFilePath: 'event/2023s-hackcloud-banner.png',
+		name: 'ACM Hack Fall GM',
+		date: getDateTime(2023, 10, 5, 18),
+		location: 'Kaplan Hall A65',
+		imgFilePath: 'event/hack-banner.png',
+		detailLink: 'https://discord.gg/rup2p6fxA5',
+		rsvpLink: 'https://forms.gle/LypLnQuQxNoeRpSV6'
+	},
+	{
+		name: 'Meet the Hack Fam Heads',
+		date: getDateTime(2023, 10, 6, 18),
+		location: 'Courtyard in front of Royce Hall',
+		imgFilePath: 'event/2023f-hackfam-banner.png',
+		detailLink: 'https://hoth.uclaacm.com/'
+	},
+	{
+		name: 'Hack Fam Head Reveal',
+		date: getDateTime(2023, 10, 9, 18),
+		location: 'Ackerman Bruin Reception Room',
+		imgFilePath: 'event/2023f-hackfam-banner.png',
+		detailLink: 'https://hoth.uclaacm.com/'
+	},
+	{
+		name: 'Arts and Crafts Social',
+		date: getDateTime(2023, 10, 18, 18),
+		location: 'Janss/Tongva Steps',
+		imgFilePath: 'event/2023f-hackfam-banner.png',
+		detailLink: 'https://hoth.uclaacm.com/'
+	},
+	{
+		name: 'Gaming Social',
+		date: getDateTime(2023, 11, 15, 18),
+		location: 'Ackerman Bruin Reception Room',
+		imgFilePath: 'event/2023f-hackfam-banner.png',
+		detailLink: 'https://hoth.uclaacm.com/'
+	},
+	{
+		name: 'Closing Ceremony',
+		date: getDateTime(2023, 12, 6, 18),
+		location: 'Ackerman Bruin Reception Room',
+		imgFilePath: 'event/2023f-hackfam-banner.png',
+		detailLink: 'https://hoth.uclaacm.com/'
+	},
+	{
+		name: 'Hackschool #1: Intro to HTML/CSS/JS',
+		date: getDateTime(2023, 10, 12, 18),
+		location: 'Kaplan Hall A65',
+		imgFilePath: 'event/2023f-hackschool-banner.png',
 		detailLink: 'https://discord.gg/rup2p6fxA5'
 	},
 	{
-		name: 'HackCloud #2: VM Computing',
-		date: getDateTime(2023, 4, 27, 18, 30),
-		location: 'Engineering VI 289',
-		imgFilePath: 'event/2023s-hackcloud-banner.png',
+		name: 'Hackschool #2: Intro to React',
+		date: getDateTime(2023, 10, 19, 18),
+		location: 'Kaplan Hall A65',
+		imgFilePath: 'event/2023f-hackschool-banner.png',
 		detailLink: 'https://discord.gg/rup2p6fxA5'
 	},
 	{
-		name: 'HackCloud #3: Serverless Computing',
-		date: getDateTime(2023, 5, 4, 19, 30),
-		location: 'Engineering VI 289',
-		imgFilePath: 'event/2023s-hackcloud-banner.png',
+		name: 'Hackschool #3: Events and State',
+		date: getDateTime(2023, 10, 26, 18),
+		location: 'Kaplan Hall A65',
+		imgFilePath: 'event/2023f-hackschool-banner.png',
 		detailLink: 'https://discord.gg/rup2p6fxA5'
 	},
 	{
-		name: 'HackCloud #4: DevOps and Cloud',
-		date: getDateTime(2023, 5, 11, 19, 30),
-		location: 'Engineering VI 289',
-		imgFilePath: 'event/2023s-hackcloud-banner.png',
+		name: 'Hackschool #4: Navigation',
+		date: getDateTime(2023, 11, 2, 18),
+		location: 'Kaplan Hall A65',
+		imgFilePath: 'event/2023f-hackschool-banner.png',
 		detailLink: 'https://discord.gg/rup2p6fxA5'
 	},
 	{
-		name: 'HackCloud #5: Machine Learning',
-		date: getDateTime(2023, 5, 18, 18, 30),
-		location: 'Engineering VI 289',
-		imgFilePath: 'event/2023s-hackcloud-banner.png',
+		name: 'Hackschool #5: Servers',
+		date: getDateTime(2023, 11, 9, 18),
+		location: 'Kaplan Hall A65',
+		imgFilePath: 'event/2023f-hackschool-banner.png',
 		detailLink: 'https://discord.gg/rup2p6fxA5'
 	},
 	{
-		name: 'HackCloud #6: Data Science',
-		date: getDateTime(2023, 5, 25, 19, 30),
-		location: 'Engineering VI 289',
-		imgFilePath: 'event/2023s-hackcloud-banner.png',
+		name: 'Hackschool #6: APIs + Copilot / GPT',
+		date: getDateTime(2023, 11, 16, 18),
+		location: 'Kaplan Hall A65',
+		imgFilePath: 'event/2023f-hackschool-banner.png',
+		detailLink: 'https://discord.gg/rup2p6fxA5'
+	},
+	{
+		name: 'Hackschool #7: Showcase',
+		date: getDateTime(2023, 11, 30, 18),
+		location: 'Kaplan Hall A65',
+		imgFilePath: 'event/2023f-hackschool-banner.png',
 		detailLink: 'https://discord.gg/rup2p6fxA5'
 	}
 ];
