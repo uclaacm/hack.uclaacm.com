a2f4b0750784a21a46f1bc5c358d534198f841a1
Nathan Zhang <nathanz.zhang@gmail.com>
2023-11-12 21:51:57 -0800

    updated rsvp and detail links

--- src/data/events.js
+++ src/data/events.js
@@ -24,35 +24,36 @@ const events = [
 		date: getDateTime(2023, 10, 6, 18),
 		location: 'Courtyard in front of Royce Hall',
 		imgFilePath: 'event/2023f-hackfam-banner.png',
-		detailLink: 'https://hoth.uclaacm.com/'
+		detailLink: 'https://discord.gg/rup2p6fxA5'
 	},
 	{
 		name: 'Hack Fam Head Reveal',
 		date: getDateTime(2023, 10, 9, 18),
 		location: 'Ackerman Bruin Reception Room',
 		imgFilePath: 'event/2023f-hackfam-banner.png',
-		detailLink: 'https://hoth.uclaacm.com/'
+		detailLink: 'https://discord.gg/rup2p6fxA5'
 	},
 	{
 		name: 'Arts and Crafts Social',
 		date: getDateTime(2023, 10, 21, 14),
 		location: 'Ouside of Saxon Lounge',
 		imgFilePath: 'event/2023f-hackfam-banner.png',
-		detailLink: 'https://hoth.uclaacm.com/'
+		detailLink: 'https://discord.gg/rup2p6fxA5'
 	},
 	{
 		name: 'Gaming Social',
 		date: getDateTime(2023, 11, 19, 14),
 		location: 'Saxon Lounge',
 		imgFilePath: 'event/2023f-hackfam-banner.png',
-		detailLink: 'https://hoth.uclaacm.com/'
+		detailLink: 'https://discord.gg/rup2p6fxA5',
+		rsvpLink: 'https://forms.gle/t8Gck1Xcg67hdV7L8'
 	},
 	{
 		name: 'Closing Ceremony',
 		date: getDateTime(2023, 12, 6, 18),
 		location: 'Ackerman Bruin Reception Room',
 		imgFilePath: 'event/2023f-hackfam-banner.png',
-		detailLink: 'https://hoth.uclaacm.com/'
+		detailLink: 'https://discord.gg/rup2p6fxA5'
 	},
 	{
 		name: 'Hackschool #1: Intro to HTML/CSS/JS',
