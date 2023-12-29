9185a64779ad5d272f8563edfe14b564b7473555
Nathan Zhang <nathanz.zhang@gmail.com>
2023-12-29 13:28:14 +0800

    testing again

--- .huskyrc.js
+++ .huskyrc.js
@@ -3,6 +3,6 @@ module.exports = {
 		// you can skip git hooks with --no-verify flag
 		// but make sure you know what you are doing
 		'pre-commit': 'yarn lint',
-		'pre-push': 'yarn lint && commands/pre-push.sh'
+		'pre-push': 'yarn lint && sh commands/pre-push.sh'
 	}
 };
