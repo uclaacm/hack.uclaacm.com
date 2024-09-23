e2d51348f6eae5d5c3fa1949a912d312e36b01db
jameswu <jamesw0462@gmail.com>
2024-09-22 20:46:54 -0700

    Added team data and added pfps and easter eggs

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,21 +1,17 @@
 import React from 'react';
 import '../styles/About.css';
-
-const aboutDescription = `We are a group of hackers, designers, and 
-engineers all working to improve UCLA's hacking community. We believe 
-in moving fast, having fun, and being passionate about using technology 
-to solve problems that are relevant to us. We have a high bar for success, 
-and are willing to work incredibly hard, balancing school and many other 
-things, to improve the experience of other students around us.`;
+import Team from '../components/About/Team';
+import { teamIntro } from '../data/profiles.js';
 
 export default function About() {
 	return (
 		<div id='about'>
 			<div className='about-header'>
 				<h1 className='about-title'>Who We Are</h1>
-				<p className='about-desc'>{aboutDescription}</p>
+				<p className='about-desc'>{teamIntro}</p>
 			</div>
 			<h2 className='about-subheader'>The Team</h2>
+			<Team />
 			<h2 className='about-subheader'>Alumni</h2>
 		</div>
 	);
