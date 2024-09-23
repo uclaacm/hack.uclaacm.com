c8491fdc9c647db399d8a516492d54c3e5d4f66c
jameswu <jamesw0462@gmail.com>
2024-09-22 21:30:56 -0700

    Events page updates

--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import '../styles/Events.css';
 
 export default function Events() {
 	return (
-		<div>
-			<p>Events</p>
+		<div id='events'>
+			<div className='events-header'>
+				<h1 className='events-title'>
+					HackEvents<sup className='sup'>TM</sup>
+				</h1>
+				<p className='events-desc'>
+					Hack offers workshops that focus on practical application, such as web
+					development and mobile development. We also host fun one-time
+					activities such as UCLA&apos;s biggest beginner-friendly Hackathon,
+					Hack on the Hill. Regardless of background or experience, you can find
+					an event that is just for you.
+				</p>
+			</div>
+			<h1>Fall 2024 Events</h1>
+			<h1>Past Events</h1>
 		</div>
 	);
 }
