f9383280b07efe201c805acbe27ca12cb33b7640
jameswu <jamesw0462@gmail.com>
2024-10-06 23:02:19 -0700

    Fixing markdown reroute issue

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: './',
-	plugins: [react()],
+	plugins: [
+		react(),
+		viteStaticCopy({
+			targets: [
+				{
+					src: 'public/blogPosts/**',
+					dest: 'blogPosts',
+				},
+			],
+		}),
+	],
 	server: {
 		port: 8000,
 	},
