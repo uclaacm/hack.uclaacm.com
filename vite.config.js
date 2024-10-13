c88fac564534ad402442853c40b7798e1eeb9ba7
jameswu <jamesw0462@gmail.com>
2024-10-13 16:22:08 -0700

    Fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { viteStaticCopy } from 'vite-plugin-static-copy';
-import yaml from '@modyfi/vite-plugin-yaml';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,7 +15,6 @@ export default defineConfig({
 				},
 			],
 		}),
-		yaml(),
 	],
 	server: {
 		port: 8000,
