474d97e3291ab6bdb61d2823264338a7b6f803fe
Nishant-Ray <raynishant1@gmail.com>
2025-04-14 22:41:41 -0700

    Added banner SVG from Figma design and implemented few animations for it

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -1,16 +1,71 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import '../../styles/Home.css';
-import leftBanner from '../../images/leftbanner.svg';
-import rightBanner from '../../images/rightbanner.svg';
-import wordmark from '../../images/logo-wordmark-gradient.svg';
+import BannerSVG from './BannerSVG';
+import { gsap } from 'gsap';
+import { MotionPathPlugin } from 'gsap/MotionPathPlugin';
+gsap.registerPlugin(MotionPathPlugin);
 
 export default function Banner() {
+  const wireRef = useRef(null);
+  const lightRef = useRef(null);
+	const [animationBegun, setAnimationBegun] = useState(false);
+
+	const defaultMotionPath = (pathId) => ({
+		motionPath: {
+			path: `#${pathId}`,
+			align: `#${pathId}`,
+			alignOrigin: [0.5, 0.5],
+			autoRotate: true
+		},
+	});
+
+	const wireIdToDuration = {
+		"wire1": 4,
+		"wire2": 5,
+		"wire3": 8,
+		"wire4": 10,
+		"wire5": 3,
+		"wire6": 2,
+		"wire7": 2,
+		"wire8": 10
+	};
+
+	const createTimeline = (lightId, wireSequence) => {
+		const timeline = gsap.timeline({ defaults: { ease: "none" }, repeat: -1, delay: 2 })
+		wireSequence.forEach(wireId => {
+			timeline.to(`#${lightId}`, {
+				...defaultMotionPath(wireId),
+				duration: wireIdToDuration[wireId],
+			});
+		});
+		return timeline;
+	};
+
+	useEffect(() => {
+		const timeline1 = createTimeline("light1", ["wire1", "wire2", "wire3", "wire4", "wire5", "wire6", "wire7", "wire8"]);
+		const timeline2 = createTimeline("light2", ["wire3", "wire4", "wire5", "wire6", "wire7", "wire8", "wire1", "wire2"]);
+		const timeline3 = createTimeline("light3", ["wire5", "wire6", "wire7", "wire8", "wire1", "wire2", "wire3", "wire4"]);
+		const timeline4 = createTimeline("light4", ["wire7", "wire8", "wire1", "wire2", "wire3", "wire4", "wire5", "wire6"]);
+
+		const animationTimer = setTimeout(() => {
+			if (lightRef.current) lightRef.current.classList.add('light-glow');
+      if (wireRef.current) wireRef.current.classList.add('wire-glow');
+			setAnimationBegun(true);
+    }, 2000);
+
+    return () => {
+			clearTimeout(animationTimer);
+			timeline1.kill;
+			timeline2.kill;
+			timeline3.kill;
+			timeline4.kill;
+		};
+  }, []);
+
 	return (
 		<div className='banner-container'>
 			<div className='banner-content'>
-				<img className='left-banner' src={leftBanner} alt='Left Banner' />
-				<img className='wordmark' src={wordmark} alt='Wordmark' />
-				<img className='right-banner' src={rightBanner} alt='Right Banner' />
+				<BannerSVG wireRef={wireRef} lightRef={lightRef} animationBegun={animationBegun}/>
 			</div>
 		</div>
 	);
