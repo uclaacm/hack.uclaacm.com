52919de21512eb82a1799e8495174b31382892ea
Kayla <kaylahamakawa@g.ucla.edu>
2025-08-23 17:49:37 -0700

    Duck moves now

--- src/components/Home/HackDescription.jsx
+++ src/components/Home/HackDescription.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useRef, useLayoutEffect } from 'react';
 import '../../styles/Home.css';
 // import pigeon from '../../images/pigeon.svg';
 import HackDescriptionSVG from './HackDescriptionSVG';
+import { gsap } from 'gsap';
 
 const hackDescription = `We are a student-run organization whose mission is to
 empower the community by providing the means to build amazing things and explore
@@ -11,8 +12,65 @@ events are for coders of all skills levels, so whether you've been to 10 hackath
 or you just learned "Hello World," we're happy to have you.`;
 
 export default function HackDescription() {
+	const root = useRef(null);
+
+	useLayoutEffect(() => {
+		const ctx = gsap.context(() => {
+			// predictable SVG transform math
+			gsap.set(['#surfer', '#duck', '#surfboard', '#eye-left', '#eye-right'], {
+				transformBox: 'fill-box',
+				transformOrigin: '50% 50%',
+			});
+			// make duck pivot a tad lower (feels like body balance)
+			gsap.set('#duck', { transformOrigin: '50% 80%' });
+
+			// master surf motion (tiny so nothing separates)
+			const tl = gsap.timeline({
+				repeat: -1,
+				yoyo: true,
+				defaults: { ease: 'sine.inOut', duration: 1.8 },
+			});
+
+			// move board+duck together
+			tl.to('#surfer', { y: -6, rotation: -2.2 }, 0);
+
+			// duck counter-rotation + teeny follow-through
+			tl.to('#duck', { y: -1.5, rotation: 1.6, delay: 0.08 }, 0);
+
+			// optional micro drift (kept very small)
+			gsap.to('#surfer', {
+				x: 4,
+				duration: 3.2,
+				ease: 'sine.inOut',
+				yoyo: true,
+				repeat: -1,
+			});
+
+			// blinks (random cadence)
+			const blink = () => {
+				gsap
+					.timeline()
+					.to(['#eye-left', '#eye-right'], {
+						scaleY: 0.12,
+						duration: 0.07,
+						ease: 'power1.in',
+						transformOrigin: '50% 50%',
+					})
+					.to(['#eye-left', '#eye-right'], {
+						scaleY: 1,
+						duration: 0.06,
+						ease: 'power1.out',
+					})
+					.add(() => gsap.delayedCall(gsap.utils.random(2, 5), blink));
+			};
+			blink();
+		}, root);
+
+		return () => ctx.revert();
+	}, []);
+
 	return (
-		<div className='hack-description-section'>
+		<div ref={root} className='hack-description-section'>
 			<div className='hack-description-content'>
 				<div className='hack-description-container'>
 					<div className='hack-description' data-aos='fade-right'>
@@ -21,7 +79,7 @@ export default function HackDescription() {
 					</div>
 				</div>
 			</div>
-			<HackDescriptionSVG className="hack-bg-svg" />
+			<HackDescriptionSVG className='hack-bg-svg' />
 		</div>
 	);
 }
