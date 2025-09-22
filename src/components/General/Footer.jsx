92bebafa4c8cbe7f2b4e1e95c11047c30c6a4781
Nishant Ray <43621741+Nishant-Ray@users.noreply.github.com>
2025-09-22 16:33:18 -0700

    Revamped footer with new design (#573)

--- src/components/General/Footer.jsx
+++ src/components/General/Footer.jsx
@@ -1,30 +1,9 @@
 import React from 'react';
-import { Facebook, Instagram, Github, MessageCircle } from '@geist-ui/icons';
 import '../../styles/Footer.css';
+import FooterSVG from './FooterSVG';
 
 export default function Footer() {
-	return (
-		<div className='footer-container'>
-			<footer className='footer'>
-				<div className='left-container'>
-					<a href='https://instagram.com/uclahack' className='icon-link'>
-						<Instagram size={32} />
-					</a>
-					<a href='https://facebook.com/acmhack' className='icon-link'>
-						<Facebook size={32} />
-					</a>
-					<a href='https://github.com/uclaacm' className='icon-link'>
-						<Github size={32} />
-					</a>
-					<a href='https://discord.gg/3GSPECbCnE' className='icon-link'>
-						<MessageCircle size={32} />
-					</a>
-				</div>
-				<div className='right-container'>
-					<div>hack@uclaacm.com</div>
-					<div>© ACM Hack</div>
-				</div>
-			</footer>
-		</div>
-	);
+	return <div className='footer-container'>
+		<FooterSVG />
+	</div>;
 }
