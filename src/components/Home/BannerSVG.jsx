1346cbfe835906715253aa0537e39dfbacf7c137
Nishant-Ray <raynishant1@gmail.com>
2025-09-29 15:32:43 -0700

    Updated banner mobile sizing

--- src/components/Home/BannerSVG.jsx
+++ src/components/Home/BannerSVG.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../styles/Home.css';
+import '../../styles/Banner.css';
 
 export default function BannerSVG({ wireRef, lightRef, textRef, animationBegun, startFlicker }) {
   return (
